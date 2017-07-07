062bcc6636239771593b9256a395c55b82e087c9
朴石煌 <shihuang.piao@jrj.com.cn>
2017-07-07 10:53:12 +0800

    build demo

--- src/demo/pages/index.js
+++ src/demo/pages/index.js
@@ -8,7 +8,7 @@ import initVue from 'utils/initVue'
 import Index from 'components/index'
 import ComponentsList from 'components/components-list'
 
-const a = 7
+const a = 8
 void (a)
 /*
  * 引入vuex
