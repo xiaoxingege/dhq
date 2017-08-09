4b3bba6f2f643399d49a69e44c98d80c24b155ea
panke.li <panke.li@jrj.com.cn>
2017-08-09 14:39:38 +0800

    加滚动的js

--- src/components/neicanms.vue
+++ src/components/neicanms.vue
@@ -1,28 +1,55 @@
 <style>
 @import '../assets/css/reset.css';
-
-.item{ background:url(../assets/images/neicanms/web_01.jpg) no-repeat center top; height: 425px; }
+.in{ width:1000px; margin:0 auto;}
+.item{ background:url(../assets/images/neicanms/web_01.jpg) no-repeat center top; height: 941px; }
 .item1{background-image:url(../assets/images/neicanms/web_01.jpg);}
-.item2{background-image:url(../assets/images/neicanms/web_02.jpg); height:340px; }
-.item3{background-image:url(../assets/images/neicanms/web_03.jpg); height:372px; }
-.item4{background-image:url(../assets/images/neicanms/web_04.jpg); height:345px; }
-.item5{background-image:url(../assets/images/neicanms/web_05.jpg); height:359px; }
-.item6{background-image:url(../assets/images/neicanms/web_06.jpg); height:425px; }
+.item2{background-image:url(../assets/images/neicanms/web_02.jpg); height:758px; }
+.item3{background-image:url(../assets/images/neicanms/web_03.jpg); height:699px; }
+.item4{background-image:url(../assets/images/neicanms/web_04.jpg); height:840px; }
+.item5{background-image:url(../assets/images/neicanms/web_05.jpg); height:581px; }
+.item6{background-image:url(../assets/images/neicanms/web_06.jpg); height:831px; }
+.item1 .in{ padding-top: 770px;}
+.item2 .in{ padding-top: 85px; text-align: center;}
+.kuaibao{ color: #782501;font-size: 30px;padding-left: 200px;}
+.total01{ color: #f79e20;}
 </style>
 
 <template>
 <div class="nicanms">
-  <div class="item item1"></div>
-  <div class="item item2"></div>
-  <div class="item item3"></div>
+  <div class="item item1">
+    <div class="in">
+      <div class="kuaibao">
+        <neicanKuaibao/>
+      </div>
+    </div>
+  </div>
+  <div class="item item2">
+    <div class="in">
+
+      <div class="total01">
+        已经有<span>100</span>人参与秒杀
+      </div>
+      <div class="timebox">
+        <ul>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  </div>
   <div class="item item3"></div>
   <div class="item item4"></div>
+  <div class="item item5"></div>
+  <div class="item item6"></div>
 </div>
 </template>
 
 <script>
+import neicanKuaibao from 'components/neican-kuaibao'
 
 export default {
+  components: {
+    neicanKuaibao
+  },
   mounted () {
     document.title = '内参秒杀'
   }
