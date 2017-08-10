6abb7ddf977bbfa5be4cbd32a5e3e4291a971b74
朴石煌 <shihuang.piao@jrj.com.cn>
2017-08-10 18:29:00 +0800

    update

--- src/z3tougu/server/routes/index.js
+++ src/z3tougu/server/routes/index.js
@@ -28,6 +28,7 @@ module.exports = function(router) {
   router.get('*', async(ctx, next) => {
     // 渲染vue对象为html字符串
     let html = '';
+    ctx.type = 'text/html';
     // 向浏览器输出完整的html
     ctx.body = html;
     // 继续执行后面的中间件
