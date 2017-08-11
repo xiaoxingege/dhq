e504af6c9744f844ed1f9fcfc514282639179618
panke.li <panke.li@jrj.com.cn>
2017-08-11 14:06:37 +0800

    内参秒杀web完成啦

--- src/components/neicanms.vue
+++ src/components/neicanms.vue
@@ -72,7 +72,6 @@
 .timebox a {
   display: block;
   height: 188px;
-  background: rgba(0, 0, 0, .5);
 }
 
 .timebox a:nth-child(2) {
@@ -109,6 +108,7 @@
 .timebox .time {
   background-position: 0 -83px;
   line-height: 20px;
+  font-size: 18px;
 padding-top: 11px;
 height: 66px;
 color: #fff;
@@ -218,7 +218,6 @@ color: #fff;
 .item4 a {
   width: 38%;
   height: 253px;
-  background: rgba(0, 0, 0, .5);
   float: left;
 }
 
@@ -240,7 +239,6 @@ color: #fff;
 .item6 a {
   display: block;
   height: 260px;
-  background: rgba(0, 0, 0, .5);
 }
 
 .item6 a:nth-child(2) {
@@ -260,7 +258,7 @@ color: #fff;
   <div class="item item2" id="one">
     <div class="in">
       <div class="total01">
-        已经有<span>100</span>人参与秒杀
+        已经有<span>{{tougjinum01}}</span>人参与秒杀
       </div>
       <div class="timebox">
         <ul class="clearfix">
@@ -270,25 +268,25 @@ color: #fff;
           <li><span class="time"></span></li>
           <li><span class="time"></span></li> -->
           <li v-for="(item,index) in urlbox.url01">
-            <a :href="item.touxiang" target="_blank"></a>
-            <a :href="item.neican" target="_blank"></a>
-            <span :class="item.classtr" v-html="item.timestr" :dataindex="index" @click="miaosha($event)"></span>
+            <a :href="item.touxiang" target="_blank" @click="tongji01"></a>
+            <a :href="item.neican" target="_blank" @click="tongji01"></a>
+            <span :class="item.classtr" v-html="item.timestr" :tgId="item.tgId" :dataindex="index" @click="miaosha($event)"></span>
           </li>
         </ul>
       </div>
     </div>
   </div>
   <div class="item item3" id="two">
-    <a href="javascript:;"></a>
+    <a href="http://itougu.jrj.com.cn/tips/10655.jspa" target="_blank"></a>
     <b id="three"></b>
-    <span>已有1000人购买</span>
+    <span>已有{{tougjinum02}}人参与购买</span>
   </div>
   <div class="item item4">
     <div class="in">
       <ul>
         <li v-for="item in urlbox.url02">
           <a :href="item.touxiang" target="_blank"></a>
-          <a :href="item.neican" target="_blank"></a>
+          <a :href="item.neican" target="_blank" @click="tongji02"></a>
         </li>
       </ul>
     </div>
@@ -299,7 +297,7 @@ color: #fff;
       <ul class="clearfix">
         <li v-for="item in urlbox.url03">
           <a :href="item.touxiang" target="_blank"></a>
-          <a :href="item.neican" target="_blank"></a>
+          <a :href="item.neican" target="_blank" @click="tongji02"></a>
         </li>
       </ul>
     </div>
@@ -318,113 +316,119 @@ color: #fff;
   <div class="layer layer_guoqi" v-if="layerguoqishow ===true ">
     <i class="closebtn" @click="closelayer"></i>
   </div>
+
 </div>
 </template>
 
 <script>
 import neicanKuaibao from 'components/neican-kuaibao'
+import {
+  mapState
+} from 'vuex'
 
 export default {
   data () {
     return {
-      opentime: '2017-08-10 15:50:36',
+      opentime: '2017-08-08 13:53:00',
       layerczshow: false,
       layerguoqishow: false,
+      tougjinum01: 0,
+      tougjinum02: 0,
       urlbox: {
         url01: [{
-          touxiang: 'http://touxing.com.cn',
-          neican: 'http://neican.com.cn',
+          touxiang: 'http://itougu.jrj.com.cn/live/26',
+          neican: 'http://itougu.jrj.com.cn/tips/9801.jspa',
           classtr: '',
           timestr: '',
-          guoqi: ''
+          tgId: '130718010073409548'
         },
         {
-          touxiang: 'http://touxing2.com.cn',
-          neican: 'http://neican3.com.cn',
+          touxiang: 'http://itougu.jrj.com.cn/live/611',
+          neican: 'http://itougu.jrj.com.cn/tips/10447.jspa',
           classtr: '',
           timestr: '',
-          guoqi: ''
+          tgId: '141120010079383950'
         },
         {
-          touxiang: 'http://tou3333xin4g.com.cn',
-          neican: 'http://mei3can.com.cn',
+          touxiang: 'http://itougu.jrj.com.cn/live/117',
+          neican: 'http://itougu.jrj.com.cn/tips/9847.jspa',
           classtr: '',
           timestr: '',
-          guoqi: ''
+          tgId: '120217010043425631'
         },
         {
-          touxiang: 'http://touxin4guxing.com.cn',
-          neican: 'http://m4eican.com.cn',
+          touxiang: 'http://itougu.jrj.com.cn/live/70',
+          neican: 'http://itougu.jrj.com.cn/tips/10150.jspa',
           classtr: '',
           timestr: '',
-          guoqi: ''
+          tgId: '091124010076736396'
         },
         {
-          touxiang: 'http://to5uxing.com.cn',
-          neican: 'http://mei6can.com.cn',
+          touxiang: 'http://itougu.jrj.com.cn/live/1238',
+          neican: 'http://itougu.jrj.com.cn/tips/10565.jspa',
           classtr: '',
           timestr: '',
-          guoqi: ''
+          tgId: '141218010033191581'
         }
         ],
         url02: [
           {
-            touxiang: 'http://touxing.com.cn',
-            neican: 'http://neican.com.cn'
+            touxiang: 'http://itougu.jrj.com.cn/live/62',
+            neican: 'http://itougu.jrj.com.cn/tips/10661.jspa'
           },
           {
-            touxiang: 'http://touxing2.com.cn',
-            neican: 'http://neican3.com.cn'
+            touxiang: 'http://itougu.jrj.com.cn/live/23',
+            neican: 'http://itougu.jrj.com.cn/tips/10673.jspa'
           },
           {
-            touxiang: 'http://tou3333xin4g.com.cn',
-            neican: 'http://mei3can.com.cn'
+            touxiang: 'http://itougu.jrj.com.cn/live/129',
+            neican: 'http://itougu.jrj.com.cn/tips/10659.jspa'
           },
           {
-            touxiang: 'http://touxin4guxing.com.cn',
-            neican: 'http://m4eican.com.cn'
+            touxiang: 'http://itougu.jrj.com.cn/live/239',
+            neican: 'http://itougu.jrj.com.cn/tips/10675.jspa'
           },
           {
-            touxiang: 'http://to5uxing.com.cn',
-            neican: 'http://mei6can.com.cn'
+            touxiang: 'http://itougu.jrj.com.cn/live/1072',
+            neican: 'http://itougu.jrj.com.cn/tips/10656.jspa'
           },
           {
-            touxiang: 'http://touxing.com.cn',
-            neican: 'http://neican.com.cn'
+            touxiang: 'http://itougu.jrj.com.cn/live/125',
+            neican: 'http://itougu.jrj.com.cn/tips/10671.jspa'
           },
           {
-            touxiang: 'http://touxing2.com.cn',
-            neican: 'http://neican3.com.cn'
+            touxiang: 'http://itougu.jrj.com.cn/live/669',
+            neican: 'http://itougu.jrj.com.cn/tips/10667.jspa'
           },
           {
-            touxiang: 'http://tou3333xin4g.com.cn',
-            neican: 'http://mei3can.com.cn'
+            touxiang: 'http://itougu.jrj.com.cn/live/2561',
+            neican: 'http://itougu.jrj.com.cn/tips/10662.jspa'
           },
           {
-            touxiang: 'http://touxin4guxing.com.cn',
-            neican: 'http://m4eican.com.cn'
+            touxiang: 'http://itougu.jrj.com.cn/live/43',
+            neican: 'http://itougu.jrj.com.cn/tips/10664.jspa'
           },
           {
-            touxiang: 'http://to5uxing.com.cn',
-            neican: 'http://mei6can.com.cn'
+            touxiang: 'http://itougu.jrj.com.cn/live/144',
+            neican: 'http://itougu.jrj.com.cn/tips/10672.jspa'
           }
         ],
         url03: [
           {
-            touxiang: 'http://touxing.com.cn',
-            neican: 'http://neican.com.cn'
+            touxiang: 'http://itougu.jrj.com.cn/live/1198',
+            neican: 'http://itougu.jrj.com.cn/tips/10668.jspa'
           },
           {
-            touxiang: 'http://touxing2.com.cn',
-            neican: 'http://neican3.com.cn'
+            touxiang: 'http://itougu.jrj.com.cn/live/741',
+            neican: 'http://itougu.jrj.com.cn/tips/10674.jspa'
           },
           {
-            touxiang: 'http://tou3333xin4g.com.cn',
-            neican: 'http://mei3can.com.cn'
+            touxiang: 'http://itougu.jrj.com.cn/live/194',
+            neican: 'http://itougu.jrj.com.cn/tips/10663.jspa'
           },
           {
-            touxiang: 'http://touxin4guxing.com.cn',
-            neican: 'http://m4eican.com.cn'
+            touxiang: 'http://itougu.jrj.com.cn/live/68',
+            neican: 'http://itougu.jrj.com.cn/tips/10658.jspa'
           }
         ]
       }
@@ -433,52 +437,101 @@ export default {
   components: {
     neicanKuaibao
   },
+  computed: mapState({
+    ssoId: state => state.user.ssoId
+    // ssoId: state => '141122010023949343'
+  }),
   mounted () {
+    this.$store.dispatch('user/fetch')
     document.title = '内参秒杀'
     this.timebtn()
+    console.log(this.ssoId)
     setInterval(this.timebtn, 1000)
+    this.tongji01()
+    this.tongji02()
   },
   methods: {
     timebtn () {
       var now = new Date()
-      console.log(now)
       var year = this.opentime.split('-')[0]
       var month = this.opentime.split('-')[1]
       var day = this.opentime.split('-')[2].split(' ')[0]
       var hour = this.opentime.split(' ')[1].split(':')[0]
-      var endDate = new Date(year, month - 1, day, hour, 0, 0)
+      var min = this.opentime.split(' ')[1].split(':')[1]
+      var se = this.opentime.split(' ')[1].split(':')[2]
+      var endDate = new Date(year, month - 1, day, hour, min, se)
 
       this.urlbox.url01.forEach(function (item, index) {
         var leftTime = endDate.getTime() - now.getTime()
+
         leftTime = leftTime + 86400000 * index
         var leftsecond = parseInt(leftTime / 1000)
         var day1 = Math.floor(leftsecond / (60 * 60 * 24))
         var hour = Math.floor((leftsecond - day1 * 24 * 60 * 60) / 3600)
         var minute = Math.floor((leftsecond - day1 * 24 * 60 * 60 - hour * 3600) / 60)
         var second = Math.floor(leftsecond - day1 * 24 * 60 * 60 - hour * 3600 - minute * 60)
-        console.log(minute)
-        if (leftTime <= 0 && item.guoqi === true) {
+
+        if (leftTime < 0) {
           item.classtr = 'gray'
           item.timestr = ''
-        } else if (leftTime <= 0) {
+        }
+        if (leftTime < 0 && leftTime > -50400000) {
           item.classtr = 'open'
           item.timestr = ''
-        } else {
+        }
+        if (leftTime > 0) {
           item.classtr = 'time'
           item.timestr = '倒计时<br />' + day1 + '天' + hour + '小时' + minute + '分' + second + '秒'
         }
       })
     },
     miaosha (v) {
-      console.log(v.currentTarget.getAttribute('dataindex'))
+      this.tongji01()
       if (v.currentTarget.className === 'gray') {
         this.layerguoqishow = true
+      } else if (v.currentTarget.className === 'open') {
+        if ((this.ssoId + '').length < 18) {
+          window.location.href = 'https://sso.jrj.com.cn/sso/ssopassportlogin?ReturnURL=' + location.href + '&fromId=JRJ'
+        } else {
+          var tgId = v.currentTarget.getAttribute('tgid')
+          fetch('http://itougu.jrj.com.cn/marketing/secondKillTips.jspa?adviserId=' + tgId + '&userId=' + this.ssoId).then((res) => {
+            return res.json()
+          }).then(v => {
+            console.log(v)
+            if (v.retCode === 0) {
+              this.layerczshow = true
+            } else {
+              this.layerguoqishow = true
+            }
+          }).catch(v2 => {
+            console.log(v2)
+          })
+        }
       }
     },
     closelayer () {
       this.layerguoqishow = false
       this.layerczshow = false
+    },
+    tongji01 () {
+      fetch('http://itougu.jrj.com.cn/marketing/secondKillAndViewTipsNum.jspa').then((res) => {
+        return res.json()
+      }).then(v => {
+        this.tougjinum01 = v.num
+      }).catch(v2 => {
+        console.log(v2)
+      })
+    },
+    tongji02 () {
+      fetch('http://itougu.jrj.com.cn/marketing/secondKillBuyTipsNum.jspa').then((res) => {
+        return res.json()
+      }).then(v => {
+        this.tougjinum02 = v.num
+      }).catch(v2 => {
+        console.log(v2)
+      })
     }
+
   }
 }
 </script>
