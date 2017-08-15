fd20e927b5a99b489796a8fa40a5b93e72d29ebb
zoule <le.zou@jrj.com.cn>
2017-08-15 14:04:26 +0800

    z3tougu.config

---
+++ src/z3tougu/config/index.js
@@ -0,0 +1,10 @@
+export default {
+  upColor: '#ef232a',
+  downColor: '#14b143',
+  flatColor: '#666',
+  ma20Color: '',
+  ma60Color: '',
+  ma120Color: ''
+}
+const domain = 'http://www.z3quant.com'
+export { domain }
