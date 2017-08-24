1b1ac4672f360d718c3e8365023dddc7ea374758
haiyuan.song <haiyuan.song@jrj.com.cn>
2017-08-24 20:21:52 +0800

    默认的

--- src/z3tougu/store/index.js
+++ src/z3tougu/store/index.js
@@ -8,7 +8,7 @@ import topic from 'stores/z3tougu-theme'
 import bubbles from 'stores/bubbles'
 import stock from 'stores/stock'
 import z3sockjs from 'stores/z3sockjs'
-/* import goldStrategy from 'stores/gold-strategy'*/
+import goldStrategy from 'stores/gold-strategy'
 
 Vue.use(Vuex)
 export default new Vuex.Store({
@@ -20,8 +20,7 @@ export default new Vuex.Store({
     topic,
     bubbles,
     stock,
-    z3sockjs
-    /*,
-    goldStrategy*/
+    z3sockjs,
+    goldStrategy
   }
 })
