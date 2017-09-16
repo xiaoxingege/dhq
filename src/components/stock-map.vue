d0877a8112d3a72ba4ac9b16d2ca1554d18dd569
haiyuan.song <haiyuan.song@jrj.com.cn>
2017-09-16 14:23:26 +0800

    默认的

--- src/components/stock-map.vue
+++ src/components/stock-map.vue
@@ -707,7 +707,7 @@
                   this.playBackIndex++
                 })
               }
-            }, 500)
+            }, 250)
           }
         },
         fmtraneValue: function (arr, n) {
