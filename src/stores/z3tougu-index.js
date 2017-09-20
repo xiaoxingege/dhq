6135a56319f97af44cc9af97cb11864ab3a9103d
haiyuan.song <haiyuan.song@jrj.com.cn>
2017-09-20 10:47:17 +0800

    fix

--- src/stores/z3tougu-index.js
+++ src/stores/z3tougu-index.js
@@ -13,44 +13,51 @@ export default {
     newsDetails: null,
     homeMapData: [],
     homeRangeData: null,
-    strategyBlock: null
+    strategyBlock: null,
+    incomeListData: []
   },
   mutations: {
-    setStrategyList (state, options) {
+    setStrategyList(state, options) {
       const result = options.result
       if (result.errCode === 0) {
         state.strategyList = result.data
       }
     },
-    setStrategyBlock (state, options) {
+    setStrategyBlock(state, options) {
       const result = options.result
       if (result.errCode === 0) {
         state.strategyBlock = result.data
       }
     },
-    setFinanceNews (state, options) {
+    setIncomeList(state, options) {
+      const result = options.result
+      if (result.errCode === 0) {
+        state.incomeListData = result.data
+      }
+    },
+    setFinanceNews(state, options) {
       const result = options.result
       state.financeNewsList = result.data
     },
-    setListedCompanyNews (state, options) {
+    setListedCompanyNews(state, options) {
       const result = options.result
       state.listedCompanyNewsList = result.data
     },
-    setNewsDetails (state, options) {
+    setNewsDetails(state, options) {
       const result = options.result
       state.newsDetails = result.data
     },
-    setHomeMapData (state, options) {
+    setHomeMapData(state, options) {
       const result = options.result
       state.homeMapData = result.voList
     },
-    setHomeRangeData (state, options) {
+    setHomeRangeData(state, options) {
       const result = options.result
       state.homeRangeData = result.data
     }
   },
   actions: {
-    getStrategyList ({
+    getStrategyList({
       commit
     }, {
       sort,
@@ -66,7 +73,7 @@ export default {
         })
       })
     },
-    getStrategyBlock ({
+    getStrategyBlock({
       commit
     }, {
       query,
@@ -82,7 +89,21 @@ export default {
         })
       })
     },
-    getFinanceNews ({
+    getIncomeList({
+      commit
+    }, {
+      strategyId
+    }) {
+      const url = `${domain}/openapi/backtest/goldStrategy/returns.shtml?strategyId=${strategyId}`
+      return fetch(url).then((res) => {
+        return res.json()
+      }).then((body) => {
+        commit('setIncomeList', {
+          result: body
+        })
+      })
+    },
+    getFinanceNews({
       commit
     }, {
       size
@@ -100,7 +121,7 @@ export default {
         })
       })
     },
-    getListedCompanyNews ({
+    getListedCompanyNews({
       commit
     }, {
       size
@@ -117,7 +138,7 @@ export default {
         })
       })
     },
-    getNewsDetails ({
+    getNewsDetails({
       commit
     }, {
       newsId
@@ -133,7 +154,7 @@ export default {
         })
       })
     },
-    getHomeMapData ({
+    getHomeMapData({
       commit
     }, {
       date
@@ -151,7 +172,7 @@ export default {
         })
       })
     },
-    getHomeRangeData ({
+    getHomeRangeData({
       commit
     }) {
       const url = `${domain}/openapi/tx/chg/`
