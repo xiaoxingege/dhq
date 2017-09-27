ec20c2184da36c524ef8ba1011d49655a951ee6b
朴石煌 <shihuang.piao@jrj.com.cn>
2017-09-27 15:21:16 +0800

    build h5 production

--- src/components/pre-pay.vue
+++ src/components/pre-pay.vue
@@ -31,23 +31,23 @@ export default {
           const cpUrl = 'http://i.jrj.com.cn/home/app/fxcpNotes?layer=0&ReturnURL=' + encodeURIComponent(payUrl)
           const idUrl = 'http://i.jrj.com.cn/home/app/nameIdentity?ReturnURL=' + encodeURIComponent(cpUrl)
           const phoneUrl = 'http://i.jrj.com.cn/home/app/phoneIdentity?ReturnURL=' + encodeURIComponent(idUrl)
-          location.href = phoneUrl
+          location.replace(phoneUrl)
         } else if (!this.bindingMobile && !this.bindingIdentity && this.riskAssessed) {
           // 未绑定手机号未绑定实名信息已测评
           const idUrl = 'http://i.jrj.com.cn/home/app/nameIdentity?ReturnURL=' + encodeURIComponent(payUrl)
           const phoneUrl = 'http://i.jrj.com.cn/home/app/phoneIdentity?ReturnURL=' + encodeURIComponent(idUrl)
-          location.href = phoneUrl
+          location.replace(phoneUrl)
         } else if (this.bindingMobile && !this.bindingIdentity && !this.riskAssessed) {
           // 已绑定手机号未绑定实名信息未测评
           const cpUrl = 'http://i.jrj.com.cn/home/app/fxcpNotes?layer=0&ReturnURL=' + encodeURIComponent(payUrl)
           const idUrl = 'http://i.jrj.com.cn/home/app/nameIdentity?ReturnURL=' + encodeURIComponent(cpUrl)
-          location.href = idUrl
+          location.replace(idUrl)
         } else if (this.bindingMobile && this.bindingIdentity && !this.riskAssessed) {
           // 已绑手机号已验证实名未测评
           const cpUrl = 'http://i.jrj.com.cn/home/app/fxcpNotes?layer=0&ReturnURL=' + encodeURIComponent(payUrl)
-          location.href = cpUrl
+          location.replace(cpUrl)
         } else if (this.bindingMobile && this.bindingIdentity && this.riskAssessed) {
-          location.href = payUrl
+          location.replace(payUrl)
         }
       }, 0)
     })
