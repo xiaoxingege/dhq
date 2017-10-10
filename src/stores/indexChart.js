3669072c2f96436440fd3aa5027f747d60d54e5d
weiling.tian <weiling.tian@jrj.com.cn>
2017-10-10 16:50:43 +0800

    fix bug

--- src/stores/indexChart.js
+++ src/stores/indexChart.js
@@ -17,44 +17,74 @@ export default {
     barData: null
   },
   mutations: {
-    setSzzsChartData (state, result) {
+    setSzzsChartData(state, result) {
       if (result.errCode === 0) {
         state.chartData.szzsChartData = result.data
       } else {
         state.chartData.szzsChartData = null
       }
     },
-    setLsChartData (state, result) {
+    setLsChartData(state, result) {
       if (result.errCode === 0) {
         state.chartData.lsChartData = result.data
       } else {
         state.chartData.lsChartData = null
       }
     },
-    setSzczChartData (state, result) {
+    setSzczChartData(state, result) {
       if (result.errCode === 0) {
         state.chartData.szczChartData = result.data
       } else {
         state.chartData.szczChartData = null
       }
     },
-    setCybzChartData (state, result) {
+    setCybzChartData(state, result) {
       if (result.errCode === 0) {
         state.chartData.cybzChartData = result.data
       } else {
         state.chartData.cybzChartData = null
       }
     },
-    setBarData (state, result) {
+    setBarData(state, result) {
       if (result.errCode === 0) {
         state.barData = result.data
       } else {
         state.barData = null
       }
+    },
+    chartSocket(state, result) {
+      if (result.stockCode === '000001.SH') {
+        state.chartData.szzsChartData.avgArr[result.list[0].time] = result.list[0].avgpx
+        state.chartData.szzsChartData.priceArr[result.list[0].time] = result.list[0].lastpx
+        state.chartData.szzsChartData.upDown = result.list[0].pxchg
+        state.chartData.szzsChartData.upDownExtent = result.list[0].pxchgRatio
+        state.chartData.szzsChartData.stockVal = result.list[0].lastpx
+      } else if (result.stockCode === '000300.SH') {
+        state.chartData.lsChartData.avgArr[result.list[0].time] = result.list[0].avgpx
+        state.chartData.lsChartData.priceArr[result.list[0].time] = result.list[0].lastpx
+        state.chartData.lsChartData.upDown = result.list[0].pxchg
+        state.chartData.lsChartData.upDownExtent = result.list[0].pxchgRatio
+        state.chartData.lsChartData.stockVal = result.list[0].lastpx
+      } else if (result.stockCode === '399001.SZ') {
+        state.chartData.szczChartData.avgArr[result.list[0].time] = result.list[0].avgpx
+        state.chartData.szczChartData.priceArr[result.list[0].time] = result.list[0].lastpx
+        state.chartData.szczChartData.upDown = result.list[0].pxchg
+        state.chartData.szczChartData.upDownExtent = result.list[0].pxchgRatio
+        state.chartData.szczChartData.stockVal = result.list[0].lastpx
+      } else if (result.stockCode === '399006.SZ') {
+        state.chartData.cybzChartData.avgArr[result.list[0].time] = result.list[0].avgpx
+        state.chartData.cybzChartData.priceArr[result.list[0].time] = result.list[0].lastpx
+        state.chartData.cybzChartData.upDown = result.list[0].pxchg
+        state.chartData.cybzChartData.upDownExtent = result.list[0].pxchgRatio
+        state.chartData.cybzChartData.stockVal = result.list[0].lastpx
+      }
+    },
+    barSocket(state, result) {
+      state.barData = result
     }
   },
   actions: {
-    getIndexChartData ({
+    getIndexChartData({
       commit
     }, {
       stockCode
@@ -75,7 +105,7 @@ export default {
         }
       })
     },
-    getBarData ({
+    getBarData({
       commit
     }) {
       return fetch(`${domain}/openapi/bars.shtml`, {
