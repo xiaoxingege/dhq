1edbbcd8555978cd4f5be264a86a66b2892db3f1
haiyuan.song <haiyuan.song@jrj.com.cn>
2017-10-12 10:49:48 +0800

    fix bug

--- src/z3tougu/plugins/z3.js
+++ src/z3tougu/plugins/z3.js
@@ -12,24 +12,11 @@ export default {
         //let code = binding.value.code;
         let vm = vnode.context;
         let popupVm = vm.$refs[popup];
-        console.log(vnode)
-        console.log(vnode.context)
-        console.log(vnode.data.attrs.value)
-        console.log(vnode.context.$refs[popup])
         el.addEventListener('mouseover', (event) => {
-          console.log(event.currentTarget.outerHTML)
           const str = event.currentTarget.outerHTML
           const parser = new DOMParser();
           const doc = parser.parseFromString(str, "text/xml");
-          let code;
-          code = vnode.data.attrs.value
-          /*if(doc.getElementsByTagName('td').length>0){
-            code = doc.getElementsByTagName('td')[0].getAttribute('value')
-
-          }else{
-            code = doc.getElementsByTagName('span')[0].getAttribute('value')
-          }*/
-
+          const code = doc.getElementsByTagName('td')[0].getAttribute('value')
           let scrollTop = window.pageYOffset || window.scrollY;
           let scrollleft = window.pageXOffset || window.scrollX;
           const winH = window.document.body.scrollHeight;
