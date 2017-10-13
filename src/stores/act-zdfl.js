5ad3b1081b8d138dcba88b1edf80e4f72207c2f2
朴石煌 <shihuang.piao@jrj.com.cn>
2017-10-13 13:33:43 +0800

    build web

--- src/stores/act-zdfl.js
+++ src/stores/act-zdfl.js
@@ -11,25 +11,25 @@ export default {
     lotteryResult: null
   },
   mutations: {
-    setError (state, msg) {
+    setError(state, msg) {
       state.err = msg
     },
-    setLotteryInfo (state, data) {
+    setLotteryInfo(state, data) {
       state.lotteryInfo = data
     },
-    setLotteryResult (state, data) {
+    setLotteryResult(state, data) {
       state.lotteryResult = data
     }
   },
   // 浏览器环境才可以使用actions来获取数据，服务端应该用Node.js的方式获取数据后，通过mutations同步的把数据存入到store
   actions: {
-    getZPoint ({
+    getZPoint({
       commit,
       rootState
     }, options) {
       commit('setError', null)
       return $.ajax({
-        url: `http://itougu.jrj.com.cn/marketing/createZpointOperate.jspa?userId=${rootState.user.ssoId}`,
+        url: `http://itougu.jrj.com.cn/marketing/createZpointOperate.jspa?userId=${rootState.user.ssoId || ''}`,
         dataType: 'json'
       }).then(data => {
         if (data.retCode) {
@@ -39,13 +39,13 @@ export default {
         commit('setError', '网络错误，请稍后重试')
       })
     },
-    getLotteryInfo ({
+    getLotteryInfo({
       commit,
       rootState
     }, options) {
       commit('setError', null)
       return $.ajax({
-        url: `http://itougu.jrj.com.cn/marketing/zOperateNewUserIndex.jspa?userId=${rootState.user.ssoId}`,
+        url: `http://itougu.jrj.com.cn/marketing/zOperateNewUserIndex.jspa?userId=${rootState.user.ssoId || ''}`,
         dataType: 'json'
       }).then(data => {
         commit('setLotteryInfo', data)
@@ -53,13 +53,13 @@ export default {
         commit('setError', '网络错误，请稍后重试')
       })
     },
-    playLottery ({
+    playLottery({
       commit,
       rootState
     }, options) {
       commit('setError', null)
       return $.ajax({
-        url: `http://itougu.jrj.com.cn/marketing/zOperateDrawPrize.jspa?userId=${rootState.user.ssoId}`,
+        url: `http://itougu.jrj.com.cn/marketing/zOperateDrawPrize.jspa?userId=${rootState.user.ssoId || ''}`,
         dataType: 'json'
       }).then(data => {
         // if (!data.retCode) {
