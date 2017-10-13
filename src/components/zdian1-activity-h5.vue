7953fe8973ff15db173025fea0391125a22936e3
朴石煌 <shihuang.piao@jrj.com.cn>
2017-10-13 18:27:19 +0800

    build h5

--- src/components/zdian1-activity-h5.vue
+++ src/components/zdian1-activity-h5.vue
@@ -392,8 +392,8 @@ a.link2 {
       <img src="http://i0.jrjimg.cn/assets/images/zdian/zdxh.png" />
     </div>
     <div class="ui-five" id="d1">
-      <a href="http://itougu.jrj.com.cn/tips/438.jspa" class="link1" @click="grab('1')"></a>
-      <a href="http://itougu.jrj.com.cn/tips/439.jspa" class="link2" @click="grab('2')"></a>
+      <a href="http://itougu.jrj.com.cn/tips/438.jspa" class="link1" @click="grab"></a>
+      <a href="http://itougu.jrj.com.cn/tips/439.jspa" class="link2" @click="grab"></a>
     </div>
     <div class="ui-six" id="d2">
       <h5>您有 {{lotteryInfo.drawNum || 0}} 次抽奖机会</h5>
@@ -454,7 +454,7 @@ import zdian1ActivityPop1 from 'components/zdian1-activity-pop1'
 import zdian1ActivityPop2 from 'components/zdian1-activity-pop2'
 
 export default {
-  data () {
+  data() {
     return {
       listData: {
         conWidth: '980px',
@@ -467,21 +467,21 @@ export default {
         autoplay: 2000,
         autoplayDisableOnInteraction: false,
         list: [{
-          imgUrl: 'http://i0.jrjimg.cn/assets/images/zdian/1-1.jpg',
-          link: ''
-        },
-        {
-          imgUrl: 'http://i0.jrjimg.cn/assets/images/zdian/1-2.jpg',
-          link: ''
-        },
-        {
-          imgUrl: 'http://i0.jrjimg.cn/assets/images/zdian/1-3.jpg',
-          link: ''
-        },
-        {
-          imgUrl: 'http://i0.jrjimg.cn/assets/images/zdian/1-4.jpg',
-          link: ''
-        }
+            imgUrl: 'http://i0.jrjimg.cn/assets/images/zdian/1-1.jpg',
+            link: ''
+          },
+          {
+            imgUrl: 'http://i0.jrjimg.cn/assets/images/zdian/1-2.jpg',
+            link: ''
+          },
+          {
+            imgUrl: 'http://i0.jrjimg.cn/assets/images/zdian/1-3.jpg',
+            link: ''
+          },
+          {
+            imgUrl: 'http://i0.jrjimg.cn/assets/images/zdian/1-4.jpg',
+            link: ''
+          }
         ]
       },
       lotteryBoxWidth: 0,
@@ -505,20 +505,20 @@ export default {
     zdian1ActivityPop2
   },
   methods: {
-    scrollTo (n) {
+    scrollTo(n) {
       var pos = $('#d' + n).offset().top
       // 实现平滑移动 1000代表时间ms
       $('html,body').stop().animate({
         scrollTop: pos
       }, 500)
     },
-    playLottery () {
+    playLottery() {
       if (window.app.name === '{{appid}}') {
         if (window.navigator.userAgent.toLowerCase().indexOf('micromessenger') !== -1) {
           this.showShareLayer()
         } else {
           window.location = 'jrjnews://tougu?t=web&url=http://itougu.jrj.com.cn/actm/zdian1-activity'
-          setTimeout(function () {
+          setTimeout(function() {
             window.location = 'http://sjcms.jrj.com.cn/app_tg.php?channel=V4V6497Y9&tgqdcode=3Q2Y3H95'
           }, 1500)
         }
@@ -536,14 +536,18 @@ export default {
           this.prize = -1
           this.$store.dispatch('actZdfl/playLottery').then(() => {
             if (!this.err) {
-              this.prize = this.lotteryResult.prizePosition * 1
+              let prize = this.lotteryResult.prizePosition * 1
+              if (prize === 0) {
+                prize = 8
+              }
+              this.prize = prize
             }
             this.$store.dispatch('actZdfl/getLotteryInfo')
           })
         }
       }
     },
-    showLotteryResult () {
+    showLotteryResult() {
       if (this.prize > 0) {
         if (this.prize === 8) {
           this.pop2Html = '<h3>遗憾了！<br />大奖与您擦肩而过</h3><p class="fz26 cl1">继续参与现金券抢购，</p><p class="fz20 cl1">可以获得更多抽奖机会。~</p><p class="fz20 cl1">祝您投资愉快</p>'
@@ -555,28 +559,28 @@ export default {
         this.pop2Show = false
       }
     },
-    pop1Close () {
+    pop1Close() {
       this.pop1Html = ''
       this.pop1Show = false
     },
-    pop2Close () {
+    pop2Close() {
       this.pop2Html = ''
       this.pop2Show = false
     },
-    hideShareLayer () {
+    hideShareLayer() {
       this.$refs.shareLayer.style.display = 'none'
     },
-    showShareLayer () {
+    showShareLayer() {
       this.$refs.shareLayer.style.display = 'block'
       this.$refs.shareLayer.style.height = $(window).height() + 'px'
     },
-    free () {
+    free() {
       if (window.app.name === '{{appid}}') {
         if (window.navigator.userAgent.toLowerCase().indexOf('micromessenger') !== -1) {
           this.showShareLayer()
         } else {
           window.location = 'jrjnews://tougu?t=web&url=http://itougu.jrj.com.cn/actm/zdian1-activity'
-          setTimeout(function () {
+          setTimeout(function() {
             window.location = 'http://sjcms.jrj.com.cn/app_tg.php?channel=V4V6497Y9&tgqdcode=3Q2Y3H95'
           }, 1500)
         }
@@ -599,12 +603,22 @@ export default {
         }
       }
     },
-    grab (type) {
-      this.pop1Html = '<img src="http://i0.jrjimg.cn/zqt-red-1000/focus/zlhWeb/web-text3.png" /><p class="fz22 mt20 lh40">现金券将在次日发放，如遇节假日则顺延；</p><p class="fz22 lh40">此现金券可用于订阅智能炒股择时工具Z点操<br />盘，请在个人中心查看并使用。</p>'
+    grab(e) {
+      if (window.app.name === '{{appid}}') {
+        if (window.navigator.userAgent.toLowerCase().indexOf('micromessenger') !== -1) {
+          this.showShareLayer()
+        } else {
+          window.location = 'jrjnews://tougu?t=web&url=http://itougu.jrj.com.cn/actm/zdian1-activity'
+          setTimeout(function() {
+            window.location = 'http://sjcms.jrj.com.cn/app_tg.php?channel=V4V6497Y9&tgqdcode=3Q2Y3H95'
+          }, 1500)
+        }
+      }
+      // this.pop1Html = '<img src="http://i0.jrjimg.cn/zqt-red-1000/focus/zlhWeb/web-text3.png" /><p class="fz22 mt20 lh40">现金券将在次日发放，如遇节假日则顺延；</p><p class="fz22 lh40">此现金券可用于订阅智能炒股择时工具Z点操<br />盘，请在个人中心查看并使用。</p>'
       // this.pop1Show = true
     }
   },
-  mounted () {
+  mounted() {
     document.title = 'Z点操盘送三大福利-金融界'
     this.$store.dispatch('user/checkLogin').then(() => {
       this.$store.dispatch('actZdfl/getLotteryInfo')
@@ -616,7 +630,7 @@ export default {
       this.lotteryBoxWidth = $(this.$refs.lotteryBox).width()
     })
 
-    function pad (str, len) {
+    function pad(str, len) {
       str = str + ''
       if (str.length < len) {
         for (let i = 0; i < len - str.length; i++) {
@@ -626,7 +640,7 @@ export default {
       return str
     }
 
-    function ShowCountDown (year, month, day, divname) {
+    function ShowCountDown(year, month, day, divname) {
       var now = new Date()
       var endDate = new Date(year, month - 1, day)
       var leftTime = endDate.getTime() - now.getTime()
@@ -640,17 +654,17 @@ export default {
       document.getElementById('text-min').innerHTML = pad(minute, 2)
       document.getElementById('text-sec').innerHTML = pad(second, 2)
     }
-    window.setInterval(function () {
+    window.setInterval(function() {
       ShowCountDown(2017, 10, 22, 'divdown1')
     }, 1000)
     $('.nav a').click(e => {
       var index = $(e.target).attr('data-index')
       this.scrollTo(index)
     })
-    setInterval(function () {
+    setInterval(function() {
       $('.ui-six .announce-box ul').animate({
         'margin-top': '-30px'
-      }, 500, function () {
+      }, 500, function() {
         $('.ui-six .announce-box ul li:first').css({
           'font-size': '.22rem'
         })
