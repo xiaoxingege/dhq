55d29cc843d0f2fb0cbd484289ceb68513e90822
loxiao <shumei.lou@jrj.com.cn>
2017-10-16 11:39:33 +0800

    add similark

---
+++ src/components/similarK.vue
@@ -0,0 +1,37 @@
+<style>
+@import '../assets/css/reset.css';
+.similarK{
+  font-family: PingFang SC;
+  color: red;
+}
+</style>
+
+<template>
+<div class="similarK">
+  
+</div>
+</template>
+
+<script>
+import 'whatwg-fetch'
+
+export default {
+  data () {
+    return {
+      name: '你的名字'
+    }
+  },
+  beforecreated () {
+
+  },
+  created () {
+
+  },
+  mounted () {
+    document.title = '相似k线解读'
+  },
+  methods: {
+
+  }
+}
+</script>
