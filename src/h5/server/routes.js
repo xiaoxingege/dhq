683c4596022e538722378830713cb3d3b7816a1d
xiaolo <shumei.lou@jrj.com.cn>
2017-10-24 11:36:39 +0800

    add template file

--- src/h5/server/routes.js
+++ src/h5/server/routes.js
@@ -38,8 +38,8 @@ module.exports = function(router) {
     });
     router.get('/ydzxDetail', async(ctx, next) => {
         ctx.title='11';
-        ctx.header = '<link rel="stylesheet" href="http://static.yidianzixun.com/apps/ant_media_v1.0.0.5/main.css" type="text/css">';
-        ctx.footer = '<script src="http://static.yidianzixun.com/apps/ant_media_v1.0.0.5/main.js"></script>';
+        ctx.htmlHeader = '<link rel="stylesheet" href="http://static.yidianzixun.com/apps/ant_media_v1.0.0.5/main.css" type="text/css">';
+        ctx.htmlFooter = '<script src="http://static.yidianzixun.com/apps/ant_media_v1.0.0.5/main.js"></script>';
       ctx.template = ctx.path.substring(1);
       // 渲染vue对象为html字符串
       let html = '';
