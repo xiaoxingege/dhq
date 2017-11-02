5f51c39d7a1a265d76f710a5dafa124b87abe7c4
haiyuan.song <haiyuan.song@jrj.com.cn>
2017-11-02 14:12:02 +0800

    test

--- src/components/customerPortrait/portrait-detail.vue
+++ src/components/customerPortrait/portrait-detail.vue
@@ -49,82 +49,30 @@ body {
 .con-left {
   float: left;
 }
-
-.con-right {
-  float: right;
-  background-color: #141518;
-}
-
-.portrait-filter-strategy {
-  height: 24%;
-}
-
-.portrait-timing-strategy {
-  height: 39%;
-}
-
-.portrait-gold-strategy {
-  height: 37%;
-}
-
-.portrait-gold-strategy>div {
-  width: 50%;
-  height: 100%;
-  float: left;
-}
 </style>
 <template>
 <div class="portrait-wrap clearfix">
   <div class="con-left">
 
   </div>
-  <div class="con-right">
-    <div class="portrait-filter-strategy">
-
-    </div>
-    <div class="portrait-timing-strategy">
-
-    </div>
-    <div class="portrait-gold-strategy clearfix">
-      <StrategyBox :benchmarkObj="benchmarkObj" @getStrategyId="passStrategyId"></StrategyBox>
-      <tradeSignals :strategyId="strategyId"></tradeSignals>
-    </div>
-
-  </div>
+  <PortraitDetailRight></PortraitDetailRight>
 </div>
 </template>
 <script type="text/javascript">
-import StrategyBox from 'components/z3touguhome/strategy-box2'
-import tradeSignals from 'components/z3touguhome/trade-signals'
+import PortraitDetailRight from 'components/customerPortrait/portrait-detail-right'
 export default {
   data() {
-    return {
-      benchmarkObj: {
-        '000300': '沪深300',
-        '000001': '上证指数',
-        '399001': '深圳成指',
-        '399006': '创业板指',
-        '399005': '中小板指',
-        '000016': '上证50',
-        '399905': '中证500',
-        '399906': '中证800',
-        '000852': '中证1000'
-      },
-      strategyId: ''
-    }
+
   },
   props: [''],
   components: {
-    StrategyBox,
-    tradeSignals
+    PortraitDetailRight
   },
   computed: {
 
   },
   methods: {
-    passStrategyId: function(msg) {
-      this.strategyId = msg
-    }
+
   },
   mounted() {
 
