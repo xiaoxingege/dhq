849d9f67ea98306ac85c4d029651020efa155919
刘洋 <yang.liu02@jrj.com.cn>
2017-11-06 10:07:40 +0800

    代码提交

---
+++ src/h5/pages/11th-activity2.js
@@ -0,0 +1,35 @@
+/*
+ * 前端页面入口文件
+ */
+
+import Vue from 'vue'
+import Vuex from 'vuex'
+import initVue from 'utils/initVue'
+import App from 'components/11th-activity2'
+
+/*
+ * 引入vuex
+ */
+Vue.use(Vuex)
+
+/*
+ * 实例化store
+ */
+ import reservation from 'stores/reservation'
+import activity11Th from 'stores/11th-activity'
+const store = new Vuex.Store({
+  modules: {
+    reservation,
+    activity11Th
+  }
+})
+
+/*
+ * 实例化vue对象，渲染页面
+ * @store  vuex的数据仓库
+ * @component  用于渲染的组件
+ */
+initVue({
+  store,
+  component: App
+})
