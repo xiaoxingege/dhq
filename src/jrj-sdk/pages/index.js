4614c60022af1ab52d3debcbf4dc13334fb22db0
朴石煌 <shihuang.piao@jrj.com.cn>
2017-11-25 11:18:08 +0800

    build jrj-sdk

--- src/jrj-sdk/pages/index.js
+++ src/jrj-sdk/pages/index.js
@@ -1,17 +1,27 @@
 /*
  * 前端页面入口文件
  */
-const $ = require('jquery')
 
 window.jrjs = {
   checkLogin(callback) {
     if (callback) {
-      $.ajax({
-        url: 'http://itougu.jrj.com.cn/act/checkLogin'
-      }).then(data => {
-        alert(data.passportId)
-        callback(data)
-      })
+      let iframe = document.createElement('iframe')
+      iframe.src = 'http://itougu.jrj.com.cn/actm/checkLogin'
+      iframe.frameBorder = 0
+      iframe.width = 0
+      iframe.height = 0
+      document.body.appendChild(iframe)
+      iframe.onload = function(){
+        iframe.contentWindow.postMessage('getPassportId', '*')
+      }
+      if (!window.__onmessage) {
+        window.addEventListener('message', function(ev) {
+          if(ev.source !== iframe.contentWindow) return
+          let passportId = ev.data
+          callback({ passportId })
+        })
+        window.__onmessage = true
+      }
     }
   },
   login(redirectUrl) {
