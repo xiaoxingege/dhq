4137063c523100b838393a0438d69236ccc9abc4
朴石煌 <shihuang.piao@jrj.com.cn>
2017-11-29 19:04:06 +0800

    build jrj-sdk

--- src/jrj-sdk/pages/index.js
+++ src/jrj-sdk/pages/index.js
@@ -3,6 +3,17 @@
  */
 
 window.jrjs = {
+  isInJRJApp() {
+    return !!window.jrj
+  },
+  openUrlInJRJApp(url) {
+    window.location.href = "jrjnews://tougu?t=web&url=" + url;
+    setTimeout(function() {
+      if (!document.webkitHidden) {
+        window.location.href = 'http://appcms.jrj.com.cn/download.jspa?channel=transfer1&tgqdcode=transfer'
+      }
+    }, 1500);
+  },
   checkLogin(callback) {
     let user = {}
     if (callback) {
