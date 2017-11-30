6313866d52185c9972a2156022af5ad404582e5a
朴石煌 <shihuang.piao@jrj.com.cn>
2017-11-30 11:26:05 +0800

    build jrj-sdk

--- src/jrj-sdk/pages/index.js
+++ src/jrj-sdk/pages/index.js
@@ -15,43 +15,22 @@ window.jrjs = {
     }, 1500);
   },
   checkLogin(callback) {
-    let user = {}
     if (callback) {
-      let cookies = document.cookie.split(';')
-      let passportId = cookies.filter((item) => {
-        return item.trim().match(/^passportId=.*/)
-      })
-      if (passportId.length === 0) {
-        return callback(user)
-      }
-      passportId = passportId[0].trim().replace(/^passportId=/, '')
-      let accessToken = cookies.filter((item) => {
-        return item.trim().match(/^accessToken=.*/)
-      })
-      if (accessToken.length === 0) {
-        return callback(user)
-      }
-      accessToken = accessToken[0].trim().replace(/^accessToken=/, '')
-
       var xhr = new XMLHttpRequest();
       xhr.timeout = 3000;
       xhr.responseType = 'text';
-      xhr.open('GET', `http://sso.jrj.com.cn/sso/passport/checkAccessToken.jsp?passportId=${passportId}&accessToken=${accessToken}`, true);
+      xhr.open('GET', `http://itougu.jrj.com.cn/act/getClientInfo`, true);
       xhr.onload = function(e) {
         if (this.status === 200) {
           let result = JSON.parse(this.responseText);
-          if (result.resultCode === '0') {
-            user.passportId = passportId
-            user.accessToken = accessToken
-          }
-          callback(user)
+          callback(result)
         }
       };
       xhr.ontimeout = function(e) {
-        callback(user)
+        callback({})
       };
       xhr.onerror = function(e) {
-        callback(user)
+        callback({})
       };
       xhr.send(null);
     }
