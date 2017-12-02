facb5c8b3ba27a524fb9ea25a8db0fb39bd48e87
haiyuan.song <haiyuan.song@jrj.com.cn>
2017-12-02 21:33:56 +0800

    fix

--- src/components/z3touguhome/bull-stock.vue
+++ src/components/z3touguhome/bull-stock.vue
@@ -49,55 +49,10 @@
         <td>风格指数</td>
         <td>风格描述</td>
       </tr>
-      <tr>
-        <td>成长性</td>
-        <td><span v-z3-updowncolor-bg="bullStockList.length>0 && formatData(bullStockList[8].value)?'--':bullStockList[8].value" :style="{width:formatValueBg(bullStockList[8].value)*45+'px'}">{{formatValue(bullStockList[8].value)}}</span></td>
-        <td>{{bullStockList.length>0 && formatData(bullStockList[8].remark)?'--':bullStockList[8].remark}}</td>
-      </tr>
-      <tr>
-        <td>杠杆</td>
-        <td><span v-z3-updowncolor-bg="bullStockList.length>0 && formatData(bullStockList[4].value)?'--':bullStockList[4].value" :style="{width:formatValueBg(bullStockList[4].value)*45+'px'}">{{formatValue(bullStockList[4].value)}}</span></td>
-        <td>{{bullStockList.length>0 && formatData(bullStockList[4].remark)?'--':bullStockList[4].remark}}</td>
-      </tr>
-      <tr>
-        <td>流动性</td>
-        <td><span v-z3-updowncolor-bg="bullStockList.length>0 && formatData(bullStockList[7].value)?'--':bullStockList[7].value" :style="{width:formatValueBg(bullStockList[7].value)*45+'px'}">{{formatValue(bullStockList[7].value)}}</span></td>
-        <td>{{bullStockList.length>0 && formatData(bullStockList[7].remark)?'--':bullStockList[7].remark}}</td>
-      </tr>
-      <tr>
-        <td>动量</td>
-        <td><span v-z3-updowncolor-bg="bullStockList.length>0 && formatData(bullStockList[1].value)?'--':bullStockList[1].value" :style="{width:formatValueBg(bullStockList[1].value)*45+'px'}">{{formatValue(bullStockList[1].value)}}</span></td>
-        <td>{{bullStockList.length>0 && formatData(bullStockList[1].remark)?'--':bullStockList[1].remark}}</td>
-      </tr>
-      <tr>
-        <td>规模</td>
-        <td><span v-z3-updowncolor-bg="bullStockList.length>0 && formatData(bullStockList[2].value)?'--':bullStockList[2].value" :style="{width:formatValueBg(bullStockList[2].value)*45+'px'}">{{formatValue(bullStockList[2].value)}}</span></td>
-        <td>{{bullStockList.length>0 && formatData(bullStockList[2].remark)?'--':bullStockList[2].remark}}</td>
-      </tr>
-      <tr>
-        <td>估值</td>
-        <td><span v-z3-updowncolor-bg="bullStockList.length>0 && formatData(bullStockList[9].value)?'--':bullStockList[9].value" :style="{width:formatValueBg(bullStockList[9].value)*45+'px'}">{{formatValue(bullStockList[9].value)}}</span></td>
-        <td>{{bullStockList.length>0 && formatData(bullStockList[9].remark)?'--':bullStockList[9].remark}}</td>
-      </tr>
-      <tr>
-        <td>波动</td>
-        <td><span v-z3-updowncolor-bg="bullStockList.length>0 && formatData(bullStockList[3].value)?'--':bullStockList[3].value" :style="{width:formatValueBg(bullStockList[3].value)*45+'px'}">{{formatValue(bullStockList[3].value)}}</span></td>
-        <td>{{bullStockList.length>0 && formatData(bullStockList[3].remark)?'--':bullStockList[3].remark}}</td>
-      </tr>
-      <tr>
-        <td>质量</td>
-        <td><span v-z3-updowncolor-bg="bullStockList.length>0 && formatData(bullStockList[6].value)?'--':bullStockList[6].value" :style="{width:formatValueBg(bullStockList[6].value)*45+'px'}">{{formatValue(bullStockList[6].value)}}</span></td>
-        <td>{{bullStockList.length>0 && formatData(bullStockList[6].remark)?'--':bullStockList[6].remark}}</td>
-      </tr>
-      <tr>
-        <td>分析师预测</td>
-        <td><span v-z3-updowncolor-bg="bullStockList.length>0 && formatData(bullStockList[5].value)?'--':bullStockList[5].value" :style="{width:formatValueBg(bullStockList[5].value)*45+'px'}">{{formatValue(bullStockList[5].value)}}</span></td>
-        <td>{{bullStockList.length>0 && formatData(bullStockList[5].remark)?'--':bullStockList[5].remark}}</td>
-      </tr>
-      <tr>
-        <td>股东</td>
-        <td><span v-z3-updowncolor-bg="bullStockList.length>0 && formatData(bullStockList[0].value)?'--':bullStockList[0].value" :style="{width:formatValueBg(bullStockList[0].value)*45+'px'}">{{formatValue(bullStockList[0].value)}}</span></td>
-        <td>{{bullStockList.length>0 && formatData(bullStockList[0].remark)?'--':bullStockList[0].remark}}</td>
+      <tr v-for='item of bullStockList'>
+        <td>{{bullStockList.length>0 && formatData(item.cname)?'--':item.cname}}</td>
+        <td><span v-z3-updowncolor-bg="bullStockList.length>0 && formatData(item.value)?'--':item.value" :style="{width:formatValueBg(item.value)*45+'px'}">{{formatValue(item.value)}}</span></td>
+        <td>{{bullStockList.length>0 && formatData(item.remark)?'--':item.remark}}</td>
       </tr>
     </table>
   </div>
