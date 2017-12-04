240a2a801119ede89fea49fc83805ad15bfa7f9b
刘洋 <yang.liu02@jrj.com.cn>
2017-12-04 16:28:02 +0800

    build h5 production

--- src/h5/server/routes.js
+++ src/h5/server/routes.js
@@ -95,6 +95,32 @@ module.exports = function(router) {
     // 继续执行后面的中间件
     await next();
   });
+  router.get('/endYear-activity', async(ctx, next) => {
+    ctx.htmlHeader = '';
+    ctx.htmlFooter = `<script type="text/javascript">
+        (function(root) {
+            root._tt_config = true;
+            var ta = document.createElement('script'); ta.type = 'text/javascript'; ta.async = true;
+            ta.src = document.location.protocol + '//' + 's3.pstatp.com/bytecom/resource/track_log/src/toutiao-track-log.js';
+            ta.onerror = function () {
+                var request = new XMLHttpRequest();
+                var web_url = window.encodeURIComponent(window.location.href);
+                var js_url  = ta.src;
+                var url = '//ad.toutiao.com/link_monitor/cdn_failed?web_url=' + web_url + '&js_url=' + js_url + '&convert_id=78777396907';
+                request.open('GET', url, true);
+                request.send(null);
+            }
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ta, s);
+        })(window);
+    </script><div tt-data-click tt-data-convertid="78777396907" tt-data-eventtype="wechat"></div>`;
+    ctx.template = ctx.path.substring(1);
+    // 渲染vue对象为html字符串
+    let html = '';
+    // 向浏览器输出完整的html
+    ctx.body = html;
+    // 继续执行后面的中间件
+    await next();
+  });
   router.get('*', async(ctx, next) => {
     ctx.template = ctx.path.substring(1);
     // 渲染vue对象为html字符串
