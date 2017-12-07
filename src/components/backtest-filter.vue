ad9ca5becf09d225546a73452b0daae916b3c847
weiling.tian <weiling.tian@jrj.com.cn>
2017-12-07 10:07:59 +0800

    fix bug

--- src/components/backtest-filter.vue
+++ src/components/backtest-filter.vue
@@ -404,11 +404,11 @@ span.copy {
         </div>
         <Pagination @getPageFromChild="goTotradePage" :totalPage="tradeTotalPage" />
       </div>
-      <div v-if="showReturns" :style="{  minHeight: (fullHeight+180) + 'px' }">
-        <LineChartFilter :strategyId="strategyId" :height="(fullHeight+180)"></LineChartFilter>
+      <div v-if="showReturns" :style="{  minHeight: (fullHeight+79) + 'px' }">
+        <LineChartFilter :strategyId="strategyId" :height="(fullHeight+79)"></LineChartFilter>
       </div>
-      <div v-if="showSellProfit" :style="{  minHeight: (fullHeight+180) + 'px' }">
-        <OneBarFilter :strategyId="strategyId" :height="(fullHeight+180)"></OneBarFilter>
+      <div v-if="showSellProfit" :style="{  minHeight: (fullHeight+79) + 'px' }">
+        <OneBarFilter :strategyId="strategyId" :height="(fullHeight+79)"></OneBarFilter>
       </div>
     </div>
   </div>
