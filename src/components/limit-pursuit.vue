482fc926a648d4ebb9e7c9c6c80a9a262da38de5
shumei.lou <shumei.lou@jrj.com.cn>
2017-12-22 17:35:08 +0800

    init limit-pursuit

---
+++ src/components/limit-pursuit.vue
@@ -0,0 +1,36 @@
+<style>
+@import '../assets/css/reset.css';
+.limit-pursuit{
+ font-family:Microsoft Yahei, Helvetica, Hiragino Sans GB, WenQuanYi Micro Hei, sans-serif;
+}
+
+</style>
+
+<template>
+	<div class="limit-pursuit">
+    涨停追击7.2
+	</div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+
+    }
+  },
+  created () {
+    document.title = '涨停追击'
+  },
+  filters: {
+
+  },
+  methods: {
+
+  },
+  mounted () {
+
+  }
+}
+</script>
