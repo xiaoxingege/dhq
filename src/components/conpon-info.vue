7cd7e549f3e3d7542cab460e0290d5a83bd24583
颜如月 <ruyue.yan@jrj.com.cn>
2017-12-29 16:22:13 +0800

    init conpon pages

---
+++ src/components/conpon-info.vue
@@ -0,0 +1,38 @@
+<style>
+@import '../assets/css/reset.css';
+.conpon{
+  font-size:0.28rem;
+}
+</style>
+
+<template>
+<div class="conpon">
+  优惠券介绍页内容
+</div>
+</template>
+
+<script>
+import 'whatwg-fetch'
+
+export default {
+  data () {
+    return {
+      name: '你的名字'
+    }
+  },
+  beforecreated () {
+
+  },
+  created () {
+
+  },
+  mounted () {
+    document.title = '相似k线解读'
+  },
+  methods: {
+    toiphone(){
+    alert('拨打134567890')
+    }
+  }
+}
+</script>
