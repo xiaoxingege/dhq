382a0f22c51c2340b5ec19743d2b9c4ffeb5dc66
朴石煌 <shihuang.piao@jrj.com.cn>
2018-01-13 23:18:21 +0800

    build h5 production

--- src/h5/server/routes.js
+++ src/h5/server/routes.js
@@ -112,6 +112,22 @@ module.exports = function(router) {
     })
     router.get('/pqng-activity', async(ctx, next) => {
       ctx.title = '加微信 送盘前牛股';
+      ctx.htmlHeader = `<script type="text/javascript">
+        (function(root) {
+            root._tt_config = true;
+            var ta = document.createElement('script'); ta.type = 'text/javascript'; ta.async = true;
+            ta.src = document.location.protocol + '//' + 's3.pstatp.com/bytecom/resource/track_log/src/toutiao-track-log.js';
+            ta.onerror = function () {
+                var request = new XMLHttpRequest();
+                var web_url = window.encodeURIComponent(window.location.href);
+                var js_url  = ta.src;
+                var url = '//ad.toutiao.com/link_monitor/cdn_failed?web_url=' + web_url + '&js_url=' + js_url + '&convert_id=78777396907';
+                request.open('GET', url, true);
+                request.send(null);
+            }
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ta, s);
+        })(window);
+      </script>`;
       ctx.metaDescription = '';
       ctx.metaKeywords = '';
       ctx.template = ctx.path.substring(1);
