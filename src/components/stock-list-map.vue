a0995fdbd9053ffd271761076ed501009783c43b
haiyuan.song <haiyuan.song@jrj.com.cn>
2018-02-02 11:24:36 +0800

    fix

--- src/components/stock-list-map.vue
+++ src/components/stock-list-map.vue
@@ -218,7 +218,7 @@ export default {
       if (this.parent && this.parent.name) {
         let avg
         if (this.parent.perf === undefined || this.parent.perf === null) {
-          avg = '';
+          avg = '--';
         } else {
           avg = this.parent.perf
         }
