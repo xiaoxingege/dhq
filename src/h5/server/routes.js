892bfa9d6fa6d68abffe8147cf0fcd7c31030705
朴石煌 <shihuang.piao@jrj.com.cn>
2018-02-02 14:48:49 +0800

    update

--- src/h5/server/routes.js
+++ src/h5/server/routes.js
@@ -24,209 +24,212 @@ const app = new Vue({
   render: h => h(ComponentsList),
   store
 })
+
 function transdate(endTime) {
-    var date = new Date();
-    date.setFullYear(endTime.substring(0, 4));
-    date.setMonth(endTime.substring(4, 6) - 1);
-    date.setDate(endTime.substring(6, 8));
-    date.setHours(endTime.substring(8, 10));
-    date.setMinutes(endTime.substring(10, 12));
-    date.setSeconds(endTime.substring(12, 14));
-    return Date.parse(date) / 1000;
+  var date = new Date();
+  date.setFullYear(endTime.substring(0, 4));
+  date.setMonth(endTime.substring(4, 6) - 1);
+  date.setDate(endTime.substring(6, 8));
+  date.setHours(endTime.substring(8, 10));
+  date.setMinutes(endTime.substring(10, 12));
+  date.setSeconds(endTime.substring(12, 14));
+  return Date.parse(date) / 1000;
 }
+
 function removeByValue(arr, val) {
-  for(var i=0; i<arr.length; i++) {
-      for(var j=0;j<val.length;j++){
-          if(arr[i] == val[j]) {
-            arr.splice(i, 1);
-            // break;
-          }
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < val.length; j++) {
+      if (arr[i] == val[j]) {
+        arr.splice(i, 1);
+        // break;
       }
+    }
   }
 }
+
 function cryptPwd(password) {
-    var md5 = crypto.createHash('md5');
-    return md5.update(password).digest('hex');
+  var md5 = crypto.createHash('md5');
+  return md5.update(password).digest('hex');
 }
 module.exports = function(router) {
-    router.get('/lottery', async(ctx, next) => {
-        let num = ctx.query.num || '20';
-        let max = ctx.query.max || '300';
-        let lmax = ctx.query.lmax || '60';
-        let level = ctx.query.level || '1';
-        let createTime = Math.round(new Date().getTime()/1000);
-        let proxy = '';
-        let proxyArr = ['','http://ss.seon.im:18778','http://47.104.145.202:8118'];
-        let lBatch = 1;
-        let dataArr = [];
-        let lotteryDataResult = await request({
-          headers:{
-            'content-type':'application/json;charset=UTF-8',
+  router.get('/lottery', async(ctx, next) => {
+    let num = ctx.query.num || '20';
+    let max = ctx.query.max || '300';
+    let lmax = ctx.query.lmax || '60';
+    let level = ctx.query.level || '1';
+    let createTime = Date.now();
+    let proxy = '';
+    let proxyArr = ['', 'http://47.104.145.202:8118', 'http://ss.seon.im:18778'];
+    let lBatch = 1;
+    let dataArr = [];
+    let lotteryDataResult = await request({
+      headers: {
+        'content-type': 'application/json;charset=UTF-8',
+      },
+      url: 'http://itougu.jrj.com.cn/act/crud/lotteryData?limit=100',
+      method: 'get'
+    });
+    lotteryDataResult = JSON.parse(lotteryDataResult)
+    if (lotteryDataResult.length > 0) {
+      for (var i = 0; i < lotteryDataResult.length; i++) {
+        dataArr = dataArr.concat(lotteryDataResult[i].lotteryData)
+        if (level === lotteryDataResult[i].level) {
+          lBatch++;
+        }
+      }
+    }
+    if (lBatch * num > lmax) {
+      ctx.body = {
+        retcode: -1,
+        msg: '此奖项已抽取完毕'
+      };
+      return;
+    }
+    for (var i = 0; i < proxyArr.length; i++) {
+      let quota = await request({
+        headers: {
+          'content-type': 'text/plain;charset=utf-8',
+        },
+        url: 'https://www.random.org/quota/?format=plain',
+        proxy: proxyArr[i],
+        method: 'get'
+      });
+      if (quota > 0) {
+        proxy = proxyArr[i]
+        break;
+      }
+    }
+    let integersResult = await request({
+      headers: {
+        'content-type': 'text/plain;charset=utf-8',
+      },
+      proxy: proxy,
+      url: `https://www.random.org/sequences/?num=${max}&min=1&max=${max}&col=1&base=10&format=plain&rnd=new`,
+      method: 'get'
+    });
+    integersResult = integersResult.split('\n')
+    removeByValue(integersResult, dataArr)
+    integersResult = integersResult.slice(0, num)
+    let parsedBody = await request({
+      headers: {
+        'content-type': 'application/json;charset=UTF-8',
+      },
+      url: 'http://itougu.jrj.com.cn/act/crud/lotteryData',
+      method: 'post',
+      json: true,
+      body: {
+        level: level,
+        lotteryData: integersResult,
+        batch: lBatch,
+        createTime: createTime,
+        key: cryptPwd(integersResult.toString() + level + lBatch + createTime)
+      }
+    })
+    ctx.body = integersResult;
+  })
+  router.get('/checkUserIsYG', async(ctx, next) => {
+    let result = await request({
+      headers: {
+        'content-type': 'application/json',
+      },
+      url: `http://crm.jrj.com/jrjcrm/callCenter/interface/checkUserIsYG.do?userId=${ctx.query.userId}`,
+      method: 'get'
+    });
+    result = JSON.parse(result)
+    if (result.retCode === 0) {
+      if (result.data.belongYG !== 0) {
+        let ssoResult = await request({
+          headers: {
+            'content-type': 'application/json',
           },
-          url: 'http://itougu.jrj.com.cn/act/crud/lotteryData?limit=30',
+          url: `http://sso.jrjc.local/sso/passport/userPassportById.jsp?passportIds=${ctx.query.userId}`,
           method: 'get'
         });
-        lotteryDataResult = JSON.parse(lotteryDataResult)
-        if(lotteryDataResult.length > 0){
-            for(var i=0;i<lotteryDataResult.length;i++){
-                dataArr = dataArr.concat(lotteryDataResult[i].lotteryData)
-                if(level === lotteryDataResult[i].level){
-                    lBatch++;
-                }
-            }
+        ssoResult = JSON.parse(ssoResult)
+        if (ssoResult.length !== 0 && ssoResult[0].bizSource.substring(0, 2) === 'YG') {
+          ctx.body = { type: true }
+        } else {
+          ctx.body = { type: false }
         }
-        if(lBatch*num > lmax){
+      } else {
+        ctx.body = { type: true }
+      }
+    } else {
+      let ssoResult = await request({
+        headers: {
+          'content-type': 'application/json',
+        },
+        url: `http://sso.jrjc.local/sso/passport/userPassportById.jsp?passportIds=${ctx.query.userId}`,
+        method: 'get'
+      });
+      ssoResult = JSON.parse(ssoResult)
+      if (ssoResult.length !== 0 && ssoResult[0].bizSource.substring(0, 2) === 'YG') {
+        ctx.body = { type: true }
+      } else {
+        ctx.body = { type: false }
+      }
+    }
+  })
+  router.get('/listByUidAndProductId', async(ctx, next) => {
+    let result = await request({
+      headers: {
+        'content-type': 'application/json',
+      },
+      url: `http://cashier.jrjc.local/order/listByUidAndProductId?uid=${ctx.query.uid}&productSubId=${ctx.query.productSubId}`,
+      method: 'get'
+    });
+    result = JSON.parse(result)
+    if (result.retCode === 0 && result.orderList.length !== 0 && Math.round(new Date().getTime() / 1000) < transdate('20171218000000')) {
+      for (var i = 0; i < result.orderList.length; i++) {
+        if (result.orderList[i].productType === parseInt(ctx.query.type)) {
+          ctx.body = {
+            type: false
+          };
+        } else {
+          if (transdate(result.orderList[i].completeTime) > transdate('20171211000000') && transdate(result.orderList[i].completeTime) < transdate('20171218000000')) {
             ctx.body = {
-                retcode:-1,
-                msg:'此奖项已抽取完毕'
+              type: true
             };
-            return;
-        }
-        for(var i=0;i<proxyArr.length;i++){
-            let quota = await request({
-              headers:{
-                'content-type':'text/plain;charset=utf-8',
-              },
-              url: 'https://www.random.org/quota/?format=plain',
-              proxy:proxyArr[i],
-              method: 'get'
-            });
-            if(quota>0){
-                proxy = proxyArr[i]
-                break;
-            }
-        }
-        let integersResult = await request({
-          headers:{
-            'content-type':'text/plain;charset=utf-8',
-          },
-          proxy: proxy,
-          url: `https://www.random.org/sequences/?num=${max}&min=1&max=${max}&col=1&base=10&format=plain&rnd=new`,
-          method: 'get'
-        });
-        integersResult = integersResult.split('\n')
-        removeByValue(integersResult,dataArr)
-        integersResult = integersResult.slice(0,num)
-        let parsedBody = await request({
-          headers:{
-            'content-type':'application/json;charset=UTF-8',
-          },
-          url: 'http://itougu.jrj.com.cn/act/crud/lotteryData',
-          method: 'post',
-          json: true,
-          body: {
-              level:level,
-              lotteryData:integersResult,
-              batch:lBatch,
-              createTime:createTime,
-              key:cryptPwd(integersResult.toString()+level+lBatch+createTime)
+          } else {
+            ctx.body = {
+              type: false
+            };
           }
-      })
-      ctx.body = integersResult;
-    })
-    router.get('/checkUserIsYG', async(ctx, next) => {
-        let result = await request({
-          headers:{
-            'content-type':'application/json',
-          },
-          url: `http://crm.jrj.com/jrjcrm/callCenter/interface/checkUserIsYG.do?userId=${ctx.query.userId}`,
-          method: 'get'
-        });
-        result = JSON.parse(result)
-        if(result.retCode === 0){
-            if(result.data.belongYG !== 0){
-                let ssoResult = await request({
-                  headers:{
-                    'content-type':'application/json',
-                  },
-                  url: `http://sso.jrjc.local/sso/passport/userPassportById.jsp?passportIds=${ctx.query.userId}`,
-                  method: 'get'
-                });
-                ssoResult = JSON.parse(ssoResult)
-                if(ssoResult.length !== 0 && ssoResult[0].bizSource.substring(0,2) === 'YG'){
-                    ctx.body = {type:true}
-                }else{
-                    ctx.body = {type:false}
-                }
-            }else{
-                ctx.body = {type:true}
-            }
-        }else {
-            let ssoResult = await request({
-              headers:{
-                'content-type':'application/json',
-              },
-              url: `http://sso.jrjc.local/sso/passport/userPassportById.jsp?passportIds=${ctx.query.userId}`,
-              method: 'get'
-            });
-            ssoResult = JSON.parse(ssoResult)
-            if(ssoResult.length !== 0 && ssoResult[0].bizSource.substring(0,2) === 'YG'){
-                ctx.body = {type:true}
-            }else{
-                ctx.body = {type:false}
-            }
         }
-    })
-    router.get('/listByUidAndProductId', async(ctx, next) => {
-        let result = await request({
-          headers:{
-            'content-type':'application/json',
-          },
-          url: `http://cashier.jrjc.local/order/listByUidAndProductId?uid=${ctx.query.uid}&productSubId=${ctx.query.productSubId}`,
-          method: 'get'
-        });
-        result = JSON.parse(result)
-        if(result.retCode === 0 && result.orderList.length !== 0 && Math.round(new Date().getTime() / 1000) < transdate('20171218000000')){
-            for(var i=0;i<result.orderList.length;i++){
-                if(result.orderList[i].productType === parseInt(ctx.query.type)){
-                    ctx.body = {
-                        type: false
-                    };
-                }else{
-                    if(transdate(result.orderList[i].completeTime) > transdate('20171211000000') && transdate(result.orderList[i].completeTime) < transdate('20171218000000')){
-                        ctx.body = {
-                            type: true
-                        };
-                    }else{
-                        ctx.body = {
-                            type: false
-                        };
-                    }
-                }
-            }
-        }else{
-            ctx.body = {
-                type: false
-            };
-        }
-    })
-    router.get('/zcpz-activity', async(ctx, next) => {
-      ctx.title = '春节钜献 资产配置礼包';
-      ctx.metaDescription = '';
-      ctx.metaKeywords = '';
-      ctx.template = ctx.path.substring(1);
-      // 渲染vue对象为html字符串
-      let html = '';
-      // 向浏览器输出完整的html
-      ctx.body = html;
-      // 继续执行后面的中间件
-      await next();
-    });
-    router.get('/sdjg-activity', async(ctx, next) => {
-      ctx.title = '金融界实地调研 直击2018A股价值王';
-      ctx.metaDescription = '';
-      ctx.metaKeywords = '';
-      ctx.template = ctx.path.substring(1);
-      // 渲染vue对象为html字符串
-      let html = '';
-      // 向浏览器输出完整的html
-      ctx.body = html;
-      // 继续执行后面的中间件
-      await next();
-    });
-    router.get('/pqng-activity', async(ctx, next) => {
-      ctx.title = '加微信 送盘前牛股';
-      ctx.htmlHeader = `<script type="text/javascript">
+      }
+    } else {
+      ctx.body = {
+        type: false
+      };
+    }
+  })
+  router.get('/zcpz-activity', async(ctx, next) => {
+    ctx.title = '春节钜献 资产配置礼包';
+    ctx.metaDescription = '';
+    ctx.metaKeywords = '';
+    ctx.template = ctx.path.substring(1);
+    // 渲染vue对象为html字符串
+    let html = '';
+    // 向浏览器输出完整的html
+    ctx.body = html;
+    // 继续执行后面的中间件
+    await next();
+  });
+  router.get('/sdjg-activity', async(ctx, next) => {
+    ctx.title = '金融界实地调研 直击2018A股价值王';
+    ctx.metaDescription = '';
+    ctx.metaKeywords = '';
+    ctx.template = ctx.path.substring(1);
+    // 渲染vue对象为html字符串
+    let html = '';
+    // 向浏览器输出完整的html
+    ctx.body = html;
+    // 继续执行后面的中间件
+    await next();
+  });
+  router.get('/pqng-activity', async(ctx, next) => {
+    ctx.title = '加微信 送盘前牛股';
+    ctx.htmlHeader = `<script type="text/javascript">
         (function(root) {
             root._tt_config = true;
             var ta = document.createElement('script'); ta.type = 'text/javascript'; ta.async = true;
@@ -242,88 +245,88 @@ module.exports = function(router) {
             var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ta, s);
         })(window);
       </script>`;
-      ctx.metaDescription = '';
-      ctx.metaKeywords = '';
-      ctx.template = ctx.path.substring(1);
-      // 渲染vue对象为html字符串
-      let html = '';
-      // 向浏览器输出完整的html
-      ctx.body = html;
-      // 继续执行后面的中间件
-      await next();
-    });
-    router.get('/zj-activity', async(ctx, next) => {
-      ctx.title = '即将涨价，限时疯抢';
-      ctx.metaDescription = '';
-      ctx.metaKeywords = '';
-      ctx.template = ctx.path.substring(1);
-      // 渲染vue对象为html字符串
-      let html = '';
-      // 向浏览器输出完整的html
-      ctx.body = html;
-      // 继续执行后面的中间件
-      await next();
-    });
-    router.get('/zdcpQxsj-activity', async(ctx, next) => {
-      ctx.title = 'Z点操盘全心升级，好礼矩献';
-      ctx.metaDescription = '';
-      ctx.metaKeywords = '';
-      ctx.template = ctx.path.substring(1);
-      // 渲染vue对象为html字符串
-      let html = '';
-      // 向浏览器输出完整的html
-      ctx.body = html;
-      // 继续执行后面的中间件
-      await next();
-    });
-    router.get('/yearEnd-activity', async(ctx, next) => {
-      ctx.title = '领航2018 布局“复兴牛”-金融界';
-      ctx.metaDescription = '';
-      ctx.metaKeywords = '';
-      ctx.template = ctx.path.substring(1);
-      // 渲染vue对象为html字符串
-      let html = '';
-      // 向浏览器输出完整的html
-      ctx.body = html;
-      // 继续执行后面的中间件
-      await next();
-    });
-    router.get('/12th-activity', async(ctx, next) => {
-      ctx.title = '双12 就要爱-金融界';
-      ctx.metaDescription = '';
-      ctx.metaKeywords = '';
-      ctx.template = ctx.path.substring(1);
-      // 渲染vue对象为html字符串
-      let html = '';
-      // 向浏览器输出完整的html
-      ctx.body = html;
-      // 继续执行后面的中间件
-      await next();
-    });
-    router.get('/12th-activity2', async(ctx, next) => {
-      ctx.title = '双12 就要爱-金融界';
-      ctx.metaDescription = '';
-      ctx.metaKeywords = '';
-      ctx.template = ctx.path.substring(1);
-      // 渲染vue对象为html字符串
-      let html = '';
-      // 向浏览器输出完整的html
-      ctx.body = html;
-      // 继续执行后面的中间件
-      await next();
-    });
-    router.get('/endYear-activity', async(ctx, next) => {
-      ctx.title = '每日3只强势股-金融界';
-      ctx.metaDescription = '';
-      ctx.metaKeywords = '';
-      ctx.template = ctx.path.substring(1);
-      // 渲染vue对象为html字符串
-      let html = '';
-      // 向浏览器输出完整的html
-      ctx.body = html;
-      // 继续执行后面的中间件
-      await next();
-    });
+    ctx.metaDescription = '';
+    ctx.metaKeywords = '';
+    ctx.template = ctx.path.substring(1);
+    // 渲染vue对象为html字符串
+    let html = '';
+    // 向浏览器输出完整的html
+    ctx.body = html;
+    // 继续执行后面的中间件
+    await next();
+  });
+  router.get('/zj-activity', async(ctx, next) => {
+    ctx.title = '即将涨价，限时疯抢';
+    ctx.metaDescription = '';
+    ctx.metaKeywords = '';
+    ctx.template = ctx.path.substring(1);
+    // 渲染vue对象为html字符串
+    let html = '';
+    // 向浏览器输出完整的html
+    ctx.body = html;
+    // 继续执行后面的中间件
+    await next();
+  });
+  router.get('/zdcpQxsj-activity', async(ctx, next) => {
+    ctx.title = 'Z点操盘全心升级，好礼矩献';
+    ctx.metaDescription = '';
+    ctx.metaKeywords = '';
+    ctx.template = ctx.path.substring(1);
+    // 渲染vue对象为html字符串
+    let html = '';
+    // 向浏览器输出完整的html
+    ctx.body = html;
+    // 继续执行后面的中间件
+    await next();
+  });
+  router.get('/yearEnd-activity', async(ctx, next) => {
+    ctx.title = '领航2018 布局“复兴牛”-金融界';
+    ctx.metaDescription = '';
+    ctx.metaKeywords = '';
+    ctx.template = ctx.path.substring(1);
+    // 渲染vue对象为html字符串
+    let html = '';
+    // 向浏览器输出完整的html
+    ctx.body = html;
+    // 继续执行后面的中间件
+    await next();
+  });
+  router.get('/12th-activity', async(ctx, next) => {
+    ctx.title = '双12 就要爱-金融界';
+    ctx.metaDescription = '';
+    ctx.metaKeywords = '';
+    ctx.template = ctx.path.substring(1);
+    // 渲染vue对象为html字符串
+    let html = '';
+    // 向浏览器输出完整的html
+    ctx.body = html;
+    // 继续执行后面的中间件
+    await next();
+  });
+  router.get('/12th-activity2', async(ctx, next) => {
+    ctx.title = '双12 就要爱-金融界';
+    ctx.metaDescription = '';
+    ctx.metaKeywords = '';
+    ctx.template = ctx.path.substring(1);
+    // 渲染vue对象为html字符串
+    let html = '';
+    // 向浏览器输出完整的html
+    ctx.body = html;
+    // 继续执行后面的中间件
+    await next();
+  });
+  router.get('/endYear-activity', async(ctx, next) => {
+    ctx.title = '每日3只强势股-金融界';
+    ctx.metaDescription = '';
+    ctx.metaKeywords = '';
+    ctx.template = ctx.path.substring(1);
+    // 渲染vue对象为html字符串
+    let html = '';
+    // 向浏览器输出完整的html
+    ctx.body = html;
+    // 继续执行后面的中间件
+    await next();
+  });
   router.get('/wangLun-activity', async(ctx, next) => {
     ctx.title = '央视嘉宾王伦全网限时回馈';
     ctx.metaDescription = '';
--- src/web/server/routes.js
+++ src/web/server/routes.js
@@ -40,6 +40,17 @@ const connectDb = async(dbName) => {
   }
 }
 
+const exceptClasses = ['11thActivity', 'koaLoggerParse']
+const privateKey = 'hello2018'
+
+const sign = function(params, t) {
+
+}
+
+const checkSign = function(params, t, sign) {
+
+}
+
 module.exports = function(router) {
   router.get('/getClientInfo', async(ctx, next) => {
     ctx.body = {
@@ -67,6 +78,14 @@ module.exports = function(router) {
   router.post('/crud/:className', async(ctx, next) => {
     let className = ctx.params.className;
     let data = ctx.request.body;
+    // let { sign, t } = ctx.request.query || {};
+    // if (exceptClasses.indexOf('className') === -1 && !checkSign(data, t, sign)) {
+    //   ctx.body = {
+    //     code: 1,
+    //     message: 'invalid sign'
+    //   }
+    //   return
+    // }
     if (!data) {
       ctx.body = {
         code: 1,
