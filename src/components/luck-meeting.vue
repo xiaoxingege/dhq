bc6986811eae258d26abe0d5b153097345ccd0e1
朴石煌 <shihuang.piao@jrj.com.cn>
2018-02-07 17:23:02 +0800

    build web production

--- src/components/luck-meeting.vue
+++ src/components/luck-meeting.vue
@@ -60,7 +60,6 @@ form {
 
 
 
-
 /* .setUp ul>li:first-child +li +li +li +li +li{width: 220px;height: 50px;border-radius: 5px;border:1px solid #e03b3a;background: #f9574d;color: #fff;line-height: 50px;text-align: center;font-size: 20px;font-weight: normal;margin-top: 20px;margin-left: 20px;background: -webkit-linear-gradient(#ff7d5f, #f65352);background: -o-linear-gradient(#ff7d5f, #f65352);background: -moz-linear-gradient(#ff7d5f, #f65352);background: linear-gradient(#ff7d5f, #f65352);} */
 
 .setUp ul>li input {
@@ -177,7 +176,11 @@ table td span {
 
   <div class="listBox">
     <div class="list">
-      <button class="submitList">导出中奖列表</button>
+      <input type="file" @change="importFile(this)" id="imFile" style="display: none" accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel" />
+      <a id="downlink"></a>
+      <!-- <button class="submitList">导出中奖列表</button> -->
+      <el-button class="submitList" style="width:210px ;height:40px; display:block;" @click="downloadFile(excelData)">导出中奖列表</el-button>
+
       <table border="1 solids #aaa" cellpadding="0" cellspacing="0">
         <tr>
           <th>奖项</th>
@@ -213,6 +216,7 @@ table td span {
 </div>
 </template>
 <script>
+var XLSX = require('xlsx')
 import md5 from 'js-md5'
 import 'whatwg-fetch'
 import {
@@ -234,7 +238,10 @@ export default {
   data() {
     return {
       priceName: [],
-      luckUser: null
+      luckUser: null,
+      fullscreenLoading: false, // 加载中
+      outFile: '', // 导出文件el
+      excelData: [] // 测试数据
     }
   },
   computed: mapState({
@@ -249,7 +256,9 @@ export default {
       let max = this.$refs.max.value;
       let num = this.$refs.num.value;
       let lmax = this.$refs.lmax.value;
+
       if (!level || !max || !num || !lmax) return alert('缺少参数')
+
       var t = new Date().getTime();
 
       let params = {
@@ -362,6 +371,7 @@ export default {
       }).then((res) => {
         res.json().then(function(data) {
           console.log(data)
+          self.excelData = data;
           self.luckUser = {}
           for (let item of data) {
             self.luckUser[item.level] = self.luckUser[item.level] || []
@@ -387,11 +397,104 @@ export default {
       }).catch(() => {
         alert('抽奖失败，请重试')
       })
+    },
+    downloadFile: function(rs) { // 点击导出按钮
+      let data = [{}]
+      for (let k in rs[0]) {
+        data[0][k] = k
+      }
+      data = data.concat(rs)
+      this.downloadExl(data, '2018年金融界年度颁奖盛典中奖名单')
+    },
+    downloadExl: function(json, downName, type) { // 导出到excel
+      let keyMap = [] // 获取键
+      for (let k in json[0]) {
+        keyMap.push(k)
+      }
+      console.info('keyMap', keyMap, json)
+      let tmpdata = [] // 用来保存转换好的json
+      json.map((v, i) => keyMap.map((k, j) => Object.assign({}, {
+        v: v[k],
+        position: (j > 25 ? this.getCharCol(j) : String.fromCharCode(65 + j)) + (i + 1)
+      }))).reduce((prev, next) => prev.concat(next)).forEach(function(v) {
+        tmpdata[v.position] = {
+          v: v.v
+        }
+      })
+      let outputPos = Object.keys(tmpdata) // 设置区域,比如表格从A1到D10
+      let tmpWB = {
+        SheetNames: ['mySheet'], // 保存的表标题
+        Sheets: {
+          'mySheet': Object.assign({},
+            tmpdata, // 内容
+            {
+              '!ref': outputPos[0] + ':' + outputPos[outputPos.length - 1] // 设置填充区域
+            })
+        }
+      }
+      let tmpDown = new Blob([this.s2ab(XLSX.write(tmpWB, {
+          bookType: (type === undefined ? 'xlsx' : type),
+          bookSST: false,
+          type: 'binary'
+        } // 这里的数据是用来定义导出的格式类型
+      ))], {
+        type: ''
+      }) // 创建二进制对象写入转换好的字节流
+      var href = URL.createObjectURL(tmpDown) // 创建对象超链接
+      this.outFile.download = downName + '.xlsx' // 下载名称
+      this.outFile.href = href // 绑定a标签
+      this.outFile.click() // 模拟点击实现下载
+      setTimeout(function() { // 延时释放
+        URL.revokeObjectURL(tmpDown) // 用URL.revokeObjectURL()来释放这个object URL
+      }, 100)
+    },
+    analyzeData: function(data) { // 此处可以解析导入数据
+      return data
+    },
+    dealFile: function(data) { // 处理导入的数据
+      console.log(data)
+      this.imFile.value = ''
+      this.fullscreenLoading = false
+      if (data.length <= 0) {
+        this.errorDialog = true
+        this.errorMsg = '请导入正确信息'
+      } else {
+        this.excelData = data
+      }
+    },
+    s2ab: function(s) { // 字符串转字符流
+      var buf = new ArrayBuffer(s.length)
+      var view = new Uint8Array(buf)
+      for (var i = 0; i !== s.length; ++i) {
+        view[i] = s.charCodeAt(i) & 0xFF
+      }
+      return buf
+    },
+    getCharCol: function(n) { // 将指定的自然数转换为26进制表示。映射关系：[0-25] -> [A-Z]。
+      let s = ''
+      let m = 0
+      while (n > 0) {
+        m = n % 26 + 1
+        s = String.fromCharCode(m + 64) + s
+        n = (n - m) / 26
+      }
+      return s
+    },
+    fixdata: function(data) { // 文件流转BinaryString
+      var o = ''
+      var l = 0
+      var w = 10240
+      for (; l < data.byteLength / w; ++l) {
+        o += String.fromCharCode.apply(null, new Uint8Array(data.slice(l * w, l * w + w)))
+      }
+      o += String.fromCharCode.apply(null, new Uint8Array(data.slice(l * w)))
+      return o
     }
 
   },
   mounted() {
     document.title = '年会抽奖后台设置页'
+    this.outFile = document.getElementById('downlink')
     this.getFormList().then(() => {
       this.getLuckUser();
     });
