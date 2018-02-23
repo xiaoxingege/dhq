105ca19f155aa25c2f065f9ed41f617ac79808e8
刘洋 <yang.liu02@jrj.com.cn>
2018-02-23 11:06:25 +0800

    build web production

--- src/components/jzxg-activity-web-new.vue
+++ src/components/jzxg-activity-web-new.vue
@@ -1,50 +1,50 @@
 <style>
 body {
-  background-color: #000 !important;
-  font-family: '微软雅黑';
+    background-color: #000 !important;
+    font-family: '微软雅黑';
 }
 
 input {
-  outline: none;
+    outline: none;
 }
 
 .bg6 .box-con .swiper-box {
-  top: 102px;
-  left: 123px;
-  margin: 0;
+    top: 102px;
+    left: 123px;
+    margin: 0;
 }
 
 .bg6 .box-con .swiper-pagination {
-  width: 233px;
-  height: 319px;
-  left: -122px;
+    width: 233px;
+    height: 319px;
+    left: -122px;
 }
 
 .bg6 .box-con .swiper-pagination span {
-  width: 233px;
-  height: 69px;
-  float: left;
-  margin: 0 0 14px;
-  background: none;
-  box-shadow: none;
+    width: 233px;
+    height: 69px;
+    float: left;
+    margin: 0 0 14px;
+    background: none;
+    box-shadow: none;
 }
 
 .pop-top h3 {
-  font-size: 36px;
-  color: #f8ce9a;
+    font-size: 36px;
+    color: #f8ce9a;
 }
 
 .fz22 {
-  font-size: 22px;
+    font-size: 22px;
 }
 
 .pop-top p {
-  color: #e3a75d;
-  line-height: 40px;
+    color: #e3a75d;
+    line-height: 40px;
 }
 
 .mt20 {
-  margin-top: 20px;
+    margin-top: 20px;
 }
 </style>
 <style lang="scss" scoped>
@@ -99,48 +99,48 @@ input {
 }
 .footer {
     width: 100%;
-    height: 210px;
+    height: 166px;
     position: fixed;
     bottom: 0;
     margin-top: -100px;
-    background: url("../assets/images/jzxg-activity/jzxg-web-new-footer-2.png") center 0 no-repeat;
+    background: url("../assets/images/jzxg-activity/jzxg-web-new-footer-3.jpg") center 0 no-repeat;
     z-index: 10;
 }
 </style>
 
 <template>
 <div class="box">
-  <div class="bg1">
-  </div>
-  <div class="bg2">
-  </div>
-  <div class="bg3">
-  </div>
-  <div class="bg4">
-  </div>
-  <div class="bg5">
-  </div>
-  <div class="bg6">
-    <div class="box-con">
-      <activity-slider :listData="listData" />
+    <div class="bg1">
     </div>
-  </div>
-  <!-- <div class="bg7">
+    <div class="bg2">
+    </div>
+    <div class="bg3">
+    </div>
+    <div class="bg4">
+    </div>
+    <div class="bg5">
+    </div>
+    <div class="bg6">
+        <div class="box-con">
+            <activity-slider :listData="listData" />
+        </div>
+    </div>
+    <!-- <div class="bg7">
     </div> -->
-  <!-- <div class="nav-fixed" @click="navFixed">
+    <!-- <div class="nav-fixed" @click="navFixed">
     </div> -->
-  <div class="footer" @click="submit">
+    <div class="footer" @click="submit">
 
-  </div>
-  <jzxg-activity-pop v-if="popShow" @close="popClose">
-    <div v-html="popHtml">
     </div>
-  </jzxg-activity-pop>
+    <jzxg-activity-pop v-if="popShow" @close="popClose">
+        <div v-html="popHtml">
+        </div>
+    </jzxg-activity-pop>
 </div>
 </template>
 <script>
 import {
-  mapState
+    mapState
 } from 'vuex'
 import activitySlider from 'components/activity-slider'
 import jzxgActivityPop from 'components/jzxg-activity-pop'
@@ -148,95 +148,95 @@ import jQuery from 'jquery'
 window.jQuery = window.$ = jQuery
 
 export default {
-  data() {
-    return {
-      listData: {
-        conWidth: '755px',
-        conHeight: '320px',
-        conPadding: '0',
-        pagShow: true,
-        arrowShow: false,
-        paginationClickable: true,
-        loop: true,
-        autoplay: 2000,
-        autoplayDisableOnInteraction: false,
-        list: [{
-            imgUrl: 'http://i0.jrjimg.cn/assets/images/zytd-1.jpg',
-            link: ''
-          },
-          {
-            imgUrl: 'http://i0.jrjimg.cn/assets/images/lxsf.jpg',
-            link: ''
-          },
-          {
-            imgUrl: 'http://i0.jrjimg.cn/assets/images/dwcl.jpg',
-            link: ''
-          },
-          {
-            imgUrl: 'http://i0.jrjimg.cn/assets/images/sjtm.jpg',
-            link: ''
-          }
-        ]
-      },
-      popHtml: '',
-      popShow: false
-    }
-  },
-  computed: mapState({
-    loginStatus: state => state.user.loginStatus,
-    riskAssessed: state => state.user.riskAssessed,
-    bindingMobile: state => state.user.bindingMobile,
-    bindingIdentity: state => state.user.bindingIdentity
-  }),
-  components: {
-    activitySlider,
-    jzxgActivityPop
-  },
-  methods: {
-    navFixed() {
-      var pos = $('.bg6').offset().top
-      // 实现平滑移动 1000代表时间ms
-      $('html,body').stop().animate({
-        scrollTop: pos
-      }, 500)
-    },
-    popClose() {
-      this.popShow = false
-      this.popHtml = ''
-      if (!this.bindingMobile) {
-        location.href = 'http://i.jrj.com.cn/home/userSetting/phoneIdentity?ReturnURL=' + encodeURIComponent(location.href)
-      } else if (!this.bindingIdentity) {
-        location.href = 'http://i.jrj.com.cn/home/userSetting/nameIdentity?ReturnURL=' + encodeURIComponent(location.href)
-      } else if (!this.riskAssessed) {
-        location.href = 'http://i.jrj.com.cn/home/userSetting/fxcp?ReturnURL=' + encodeURIComponent('http://itougu.jrj.com.cn/activity/web/groupOrderWeb.jspa#/?productSubId=100050008&type=4')
-      } else {
-        location.href = 'http://itougu.jrj.com.cn/activity/web/groupOrderWeb.jspa#/?productSubId=100050008&type=4'
-      }
-    },
-    submit() {
-      if (this.loginStatus === 'no') {
-        location.href = 'https://sso.jrj.com.cn/sso/ssopassportlogin?ReturnURL=' + encodeURIComponent(location.href)
-      } else if (this.loginStatus === 'unknown') {
-        alert('正在加载用户信息，请稍候')
-      } else {
-        if (!this.bindingMobile || !this.bindingIdentity) {
-          this.popHtml = '<h3>温馨提示</h3><p class="fz22 mt20">为响应证监会监管要求，保障投资者合法权益，请您先<br /><strong>补充真实信息后再进行购买，</strong></p><p class="fz22">如需帮助，请咨询400-166-1188</p>'
-          this.popShow = true
-        } else if (!this.riskAssessed) {
-          this.popHtml = '<h3>温馨提示</h3><p class="fz22 mt20">响应证监会监管要求，投资者购买产品需填写风险承受<br />能力评测。<strong>请您根据下方提示完成风险承受能力测评，</strong></p><p class="fz22">如需帮助，请咨询400-166-1188</p>'
-          this.popShow = true
-        } else {
-          location.href = 'http://itougu.jrj.com.cn/activity/web/groupOrderWeb.jspa#/?productSubId=100050008&type=4'
+    data() {
+        return {
+            listData: {
+                conWidth: '755px',
+                conHeight: '320px',
+                conPadding: '0',
+                pagShow: true,
+                arrowShow: false,
+                paginationClickable: true,
+                loop: true,
+                autoplay: 2000,
+                autoplayDisableOnInteraction: false,
+                list: [{
+                        imgUrl: 'http://i0.jrjimg.cn/assets/images/zytd-1.jpg',
+                        link: ''
+                    },
+                    {
+                        imgUrl: 'http://i0.jrjimg.cn/assets/images/lxsf.jpg',
+                        link: ''
+                    },
+                    {
+                        imgUrl: 'http://i0.jrjimg.cn/assets/images/dwcl.jpg',
+                        link: ''
+                    },
+                    {
+                        imgUrl: 'http://i0.jrjimg.cn/assets/images/sjtm.jpg',
+                        link: ''
+                    }
+                ]
+            },
+            popHtml: '',
+            popShow: false
         }
-      }
+    },
+    computed: mapState({
+        loginStatus: state => state.user.loginStatus,
+        riskAssessed: state => state.user.riskAssessed,
+        bindingMobile: state => state.user.bindingMobile,
+        bindingIdentity: state => state.user.bindingIdentity
+    }),
+    components: {
+        activitySlider,
+        jzxgActivityPop
+    },
+    methods: {
+        navFixed() {
+            var pos = $('.bg6').offset().top
+            // 实现平滑移动 1000代表时间ms
+            $('html,body').stop().animate({
+                scrollTop: pos
+            }, 500)
+        },
+        popClose() {
+            this.popShow = false
+            this.popHtml = ''
+            if (!this.bindingMobile) {
+                location.href = 'http://i.jrj.com.cn/home/userSetting/phoneIdentity?ReturnURL=' + encodeURIComponent(location.href)
+            } else if (!this.bindingIdentity) {
+                location.href = 'http://i.jrj.com.cn/home/userSetting/nameIdentity?ReturnURL=' + encodeURIComponent(location.href)
+            } else if (!this.riskAssessed) {
+                location.href = 'http://i.jrj.com.cn/home/userSetting/fxcp?ReturnURL=' + encodeURIComponent('http://itougu.jrj.com.cn/activity/web/groupOrderWeb.jspa#/?productSubId=100050008&type=4')
+            } else {
+                location.href = 'http://itougu.jrj.com.cn/activity/web/groupOrderWeb.jspa#/?productSubId=100050008&type=4'
+            }
+        },
+        submit() {
+            if (this.loginStatus === 'no') {
+                location.href = 'https://sso.jrj.com.cn/sso/ssopassportlogin?ReturnURL=' + encodeURIComponent(location.href)
+            } else if (this.loginStatus === 'unknown') {
+                alert('正在加载用户信息，请稍候')
+            } else {
+                if (!this.bindingMobile || !this.bindingIdentity) {
+                    this.popHtml = '<h3>温馨提示</h3><p class="fz22 mt20">为响应证监会监管要求，保障投资者合法权益，请您先<br /><strong>补充真实信息后再进行购买，</strong></p><p class="fz22">如需帮助，请咨询400-166-1188</p>'
+                    this.popShow = true
+                } else if (!this.riskAssessed) {
+                    this.popHtml = '<h3>温馨提示</h3><p class="fz22 mt20">响应证监会监管要求，投资者购买产品需填写风险承受<br />能力评测。<strong>请您根据下方提示完成风险承受能力测评，</strong></p><p class="fz22">如需帮助，请咨询400-166-1188</p>'
+                    this.popShow = true
+                } else {
+                    location.href = 'http://itougu.jrj.com.cn/activity/web/groupOrderWeb.jspa#/?productSubId=100050008&type=4'
+                }
+            }
+        }
+    },
+    mounted() {
+        document.title = '极智选股-金融界'
+        this.$store.dispatch('user/checkLogin')
+        this.$watch('loginStatus', () => {
+            this.$store.dispatch('user/checkBindingInfo', {})
+        })
     }
-  },
-  mounted() {
-    document.title = '极智选股-金融界'
-    this.$store.dispatch('user/checkLogin')
-    this.$watch('loginStatus', () => {
-      this.$store.dispatch('user/checkBindingInfo', {})
-    })
-  }
 }
 </script>
