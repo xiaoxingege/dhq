92a8cb11b30d082b692986d78f5c14438d14db71
haiyuan.song <haiyuan.song@jrj.com.cn>
2018-05-08 10:25:18 +0800

    fix

--- src/components/z3touguhome/hotnews-home.vue
+++ src/components/z3touguhome/hotnews-home.vue
@@ -52,7 +52,8 @@
   <div class="news-top">
     <NavBar :data="navText" :type="type" v-on:changeType="changeNavType"></NavBar>
     <p class="more-news">
-      <router-link :to="{name:'newslist',query:{type:type}}">更多></router-link>
+      <router-link :to="{name:'wisdomHeadlines'}" target="_blank" v-if="type === 'ztt'">更多></router-link>
+      <router-link :to="{name:'listedCompany'}" target="_blank" v-if="type === 'ssgs'">更多></router-link>
     </p>
   </div>
   <ul class="finance-news-list">
