214898bdebf3b47ba41e35eb9db2a2498db88f8c
yao.wang <wangyao0288@163.com>
2018-06-25 18:17:41 +0800

    jjrl

--- src/components/jjrl/hy-detail.vue
+++ src/components/jjrl/hy-detail.vue
@@ -1,5 +1,22 @@
 <template>
 <div>
+    <div class="listCon">
+        <span class="hyDate">05-04</span>
+        <div class="hyTitle">
+            <div>2018(第七届)巴拉巴拉变身大会</div>
+            <div>关注程度：
+                <ul>
+                    <li></li>
+                </ul>
+            </div>
+            <div>相关股票：
+                <ul>
+                    <li>    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
    
 </div>
     
@@ -29,6 +46,18 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.listCon{
+    width:83.5%;
+    box-sizing:border-box;
+    display:flex;
+    font-size:12px;
+    padding-left:10px;
+}
+.hyTitle{
+    margin-left:20px;
+}
+.hyTitle div{
+    margin-bottom:13px;
+}
 
 </style>
