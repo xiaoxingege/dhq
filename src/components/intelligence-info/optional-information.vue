d9b42c53e5b9af5f30f5fa8e2b0200f864a29c8a
xiaolin.zhao <xiaolin.zhao@genius.com.cn>
2018-07-05 16:48:12 +0800

    自选

--- src/components/intelligence-info/optional-information.vue
+++ src/components/intelligence-info/optional-information.vue
@@ -41,7 +41,12 @@
       </div>
       <p v-if="noData" class="tc loadMore">我是有下限的~</p>
     </ul>
-    <div v-if="optionalInformationList.length===0 && loadingShow != true" class="tc mt-10 noDataList"><a :href="'/SelfStockPageView/'+optionalStockId" target="_blank" class="lookStock"><img src="../../assets/images/news-img/noStock.png" alt="" /></a><p class="tc mt-10">您还没有自选股，快去<a :href="'/SelfStockPageView/'+optionalStockId" target="_blank" class="lookStock">【添加】</a>吧~</p></div>
+    <div v-if="optionalInformationList.length===0 && loadingShow != true" class="tc mt-10 noDataList">
+      <a :href="'/SelfStockPageView/'+optionalStockId" target="_blank" class="lookStock"></a>
+      <!-- <img src="../../assets/images/news-img/noStock.png" alt="" /> -->
+      <i class="addStockBg"></i>
+      <p class="tc mt-10">您还没有自选股，快去<a :href="'/SelfStockPageView/'+optionalStockId" target="_blank" class="lookStock">【添加】</a>吧~</p>
+    </div>
     <scrollTopBar :show="isBackTop" @backTop="backTop" :id="'optionalInformation'"></scrollTopBar>
   </div>
   <StockBox ref="stockbox"></StockBox>
@@ -67,7 +72,8 @@ export default {
       innerHeight: window.innerHeight,
       innerCodes: '',
       flag: true,
-      isBackTop:false
+      isBackTop:false,
+      optionalInformationList:[]
     }
   },
   mounted() {
@@ -502,6 +508,17 @@ export default {
     a{
       font-size: 14px;
       color:#009afe;
+    }
+    .addStockBg{
+      display: inline-block;
+      width: 41px;
+      height: 51px;
+      background: url('../../assets/images/news-img/noStock.png') center no-repeat;
+      cursor: pointer;
+      transition: all .2s;
+      &:hover{
+        background: url('../../assets/images/news-img/noStock_h.png') center no-repeat;
+      }
     }
 }
 .fontS14 {
