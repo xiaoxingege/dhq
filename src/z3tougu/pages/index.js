4f3ca5220604d473f1808bed79e9f1d046b6ac5a
zoule <le.zou@jrj.com.cn>
2018-07-12 15:29:45 +0800

    z3tougu:神策分析

--- src/z3tougu/pages/index.js
+++ src/z3tougu/pages/index.js
@@ -7,6 +7,21 @@ import routes from '../router'
 import store from '../store'
 import vueCookie from 'vue-cookie'
 import z3 from '../plugins/z3'
+import sa from 'sa-sdk-javascript'
+
+// 神策分析===开始
+sa.init({
+  'show_log': true,
+  'server_url': 'http://sensor.jrj.com.cn/?token=z3',
+  heatmap: {
+    // 是否开启点击图，默认 default 表示开启，自动采集 $WebClick 事件，可以设置 'not_collect' 表示关闭
+    clickmap: 'default',
+    // 是否开启触达注意力图，默认 default 表示开启，自动采集 $WebStay 事件，可以设置 'not_collect' 表示关闭
+    'scroll_notice_map': 'not_collect'
+  }
+});
+sa.quick('autoTrack');
+// 神策分析===结束
 
 Vue.use(vueCookie)
 Vue.use(z3)
