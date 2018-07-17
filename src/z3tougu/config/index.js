2660e71a655c6d615bd2f593986aa2f6ab45979e
zoule <le.zou@jrj.com.cn>
2018-07-17 15:26:52 +0800

    change test env config

--- src/z3tougu/config/index.js
+++ src/z3tougu/config/index.js
@@ -19,14 +19,14 @@ export default {
 // const env = 'dev';
 
 // 测试环境
-// const domain = '//test.z3quant.com' // openapi
-// const websocketDomain = '//test.z3quant.com' // websocket
-// const env = 'test';
+const domain = '//test.z3quant.com' // openapi
+const websocketDomain = '//test.z3quant.com' // websocket
+const env = 'test';
 
 // 生产环境
- const domain = '//www.z3quant.com' // openapi
- const websocketDomain = '//www.z3quant.com' // websocket
- const env='production';
+//  const domain = '//www.z3quant.com' // openapi
+//  const websocketDomain = '//www.z3quant.com' // websocket
+//  const env='production';
 
 const ctx = '/zstgweb'
 // const ctx = '';
