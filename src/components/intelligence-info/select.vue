4defec0384c530f187b404ceeec4b16cb3d716fc
xiaolin.zhao <xiaolin.zhao@genius.com.cn>
2018-07-26 11:06:13 +0800

    样式

---
+++ src/components/intelligence-info/select.vue
@@ -0,0 +1,42 @@
+<template>
+  <div ref="selectBox" class="select">
+    <div class="select-txt" @click="handleTit(index)">{{select}}<i class="arrows"></i></div>
+    <ul v-if="isShow" class="select-option">
+      <li v-for="(item,index) in option">
+        {{}}
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  props: ['index','isShow'],
+  data () {
+    return {
+      txt:'select',
+      option:[],
+      isShow:false,
+      entitiesState:[]
+    }
+  },
+  mounted(){
+    document.addEventListener('click', (e) => {
+      if (!this.$refs.selectBox.contains(e.target)) this.entitiesState = [false]
+    })
+  },
+  methods: {
+    handleTit(i) {
+      let  state = this.entitiesState
+      this.$set(state,!state)
+      for (let a in this.entitiesState) {
+        if(a!==i){
+          this.$set(state,a,false)
+        }
+      }
+    }
+  }
+}
+</script>
+<style lang="sass" scoped>
+
+</style>
