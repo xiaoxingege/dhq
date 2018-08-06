f99a82f97933bc84b6a2c323101494ec254d0dda
weiling.tian <827084218@qq.com>
2018-08-06 15:49:14 +0800

    四维 1.3.3 功能：牛股归因提测

--- src/components/siwei/stock-ztg.vue
+++ src/components/siwei/stock-ztg.vue
@@ -56,51 +56,6 @@
                 </li>
             </ul>
         </div>
-      <StockInit startTime='9:00' endTime='9:30' @propShow="getShow" v-if='isShow'>
-        <img src="../../assets/images/icons/wait-cup.png" alt="">
-      </StockInit>
-      <div style="height:100%;" v-else>
-        <div class="list-header">
-          <span>涨停股</span>
-          <span :style="{color: showList ? '#2388da':''}" @click="showList = true">列表</span>
-          <span class="line"></span>
-          <span :style="{color: !showList ? '#2388da':''}" @click="showList = false">文本</span>
-          <img v-show="!showList" class="fr copy" @click="copyListText" src="../../assets/images/z3img/copy.png" />
-          <textarea ref="copyText" style="opacity: 0;position: absolute;"></textarea>
-        </div>
-        <ul ref="ztgListUl" v-show="showList">
-          <li v-for="item in ztgList" class="pb-20" @dblclick="toStockDetail(item.innerCode)">
-            <div class="mb-10" v-if="String(item.tradeTime).length === 6">{{String(item.tradeTime).substring(0,2)+':'+String(item.tradeTime).substring(2,4)+':'+String(item.tradeTime).substring(4)}}</div>
-            <div class="mb-10" v-if="String(item.tradeTime).length === 5">{{String(item.tradeTime).substring(0,1)+':'+String(item.tradeTime).substring(1,3)+':'+String(item.tradeTime).substring(3)}}</div>
-            <div style="margin-bottom: 7px;" class="clearfix">
-              <div class="fl mr-20"><span style="margin-right: 2px;">{{item.name}}</span><span>[{{item.symbol.substring(0,6)}}]</span>
-              </div>
-              <div class="fl"><span v-z3-updowncolor="item.chngPct">{{item.price | decimal(2)}}</span><span class="ml-10 mr-10" v-z3-updowncolor="item.chngPct">{{item.chngPct | chngPct}}</span>
-              </div>
-              <div class="fr" style="margin-right: 5px" v-z3-updowncolor="item.moveSignalId -1.5">{{item.reasonShortLine}}</div>
-            </div>
-            <div v-if="item.title" style="margin-bottom: 5px;"><span class="markBox" v-if="item.moveSignalId === 1" style="background-color: #56a870">利空</span><span class="markBox" v-if="item.moveSignalId === 2" style="background-color: #ca4941">利好</span>{{item.title}}</div>
-            <ul class="topicStock clearfix">
-              <li v-for="value in item.topicDataList" @dblclick="toThemeDetail(value.topicCode,$event)">
-                <div class="name">{{value.topicName}}</div>
-                <div class="price" v-z3-updowncolor="value.topicChngPct">{{value.topicChngPct | chngPct}}</div>
-              </li>
-            </ul>
-          </li>
-        </ul>
-        <ul ref="ztgListUl" class="ztgListUl" v-show="!showList">
-          <li v-for="item in ztgList" class="pb-20 clearfix" @dblclick="toStockDetail(item.innerCode)">
-            <div class="mr-10 fl" v-if="String(item.tradeTime).length === 6">{{String(item.tradeTime).substring(0,2)+':'+String(item.tradeTime).substring(2,4)}}</div>
-            <div class="mr-10 fl" v-if="String(item.tradeTime).length === 5">{{String(item.tradeTime).substring(0,1)+':'+String(item.tradeTime).substring(1,3)}}</div>
-            <div style="width:90%;" class="fl">
-              <span style="margin-right: 2px;"><a @click="toStockDetail(item.symbol)">{{item.name}}</a>涨停,</span>
-              <span>{{item.limitUpDays === null ? '' : item.limitUpDays > 0 ? item.limitUpDays+'连板,' : '首板,'}}</span>
-              <span>{{item.topicDataList[0].topicName+'。'}}</span>
-              <span v-if="item.title">{{'消息面，'+item.title+'。'}}</span>
-            </div>
-          </li>
-        </ul>
-      </div>
     </div>
   </div>
   <div class="legend"></div>
