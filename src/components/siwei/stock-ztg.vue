938a2a7d662fa22d969bc142a43bab7492fdf1c2
weiling.tian <827084218@qq.com>
2018-08-15 17:02:42 +0800

    四维 1.3.3 需求：fix bug

--- src/components/siwei/stock-ztg.vue
+++ src/components/siwei/stock-ztg.vue
@@ -49,7 +49,7 @@
                     <div class="mr-10 fl" v-if="String(item.tradeTime).length === 5">{{String(item.tradeTime).substring(0,1)+':'+String(item.tradeTime).substring(1,3)}}</div>
                     <div style="width:90%;" class="fl">
                         <span style="margin-right: 2px;"><a @click="toStockDetail(item.symbol)">{{item.name}}</a>涨停<span v-if="item.topicDataList[0]">,</span><span v-else>。</span></span>
-                        <span>{{item.limitUpDays === null ? '' : item.limitUpDays > 0 ? item.limitUpDays+'连板,' : '首板,'}}</span>
+                        <span>{{item.limitUpDays === null ? '' : item.limitUpDays > 1 ? item.limitUpDays+'连板,' : '首板,'}}</span>
                         <span v-if="item.topicDataList[0]">{{item.topicDataList[0].topicName + (item.topicDataList[0].topicName.indexOf('概念') > 0 ? '。':'概念。')}}</span>
                         <span v-if="item.title">{{'消息面，'+item.title+'。'}}</span>
                     </div>
